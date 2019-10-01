d1f0ffba3ae7111bea17657b2903ad4abb792caf
Dabble <ddabble@users.noreply.github.com>
2019-10-01 10:59:16 +0200

    Added django-cors-headers

--- src/aas/site/settings/base.py
+++ src/aas/site/settings/base.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'aas.site.alert',
 
     'social_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,6 +145,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 
 AUTHENTICATION_BACKENDS = (
