7a824bef4f3abb518a729339d0376fd2276eb830
Andreas Melzer <akrimelzer@gmail.com>
2019-10-15 13:12:59 +0200

    :sparkles: add endpoint to filter alerts on source

--- src/aas/site/alert/urls.py
+++ src/aas/site/alert/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'alert'
+app_name = "alert"
 urlpatterns = [
-    path('all/', views.all_alerts_view, name='all'),
-    path('create/', login_required(views.CreateAlertView.as_view()), name='create'),
+    path("all/", views.all_alerts_view, name="all"),
+    path("create/", login_required(views.CreateAlertView.as_view()), name="create"),
+    path("source/<str:source_id>/", views.all_alerts_from_source_view, name="source"),
 ]
