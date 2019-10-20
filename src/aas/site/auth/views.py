9f60241bbb98b063dbc560282c9050d22afd08b0
Dabble <ddabble@users.noreply.github.com>
2019-10-20 22:45:11 +0200

    Added custom user serializer :sparkles:

--- src/aas/site/auth/views.py
+++ src/aas/site/auth/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
-from django.core import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_user(request):
-    data = serializers.serialize("json", [request.user])
-    return HttpResponse(data, content_type="application/json")
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
