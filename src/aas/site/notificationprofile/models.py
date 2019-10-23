4f82bd1b9ba770c78a87ad9857a4e73d3c6f2043
Dabble <ddabble@users.noreply.github.com>
2019-10-23 04:10:44 +0200

    Added notification media field to profile :sparkles:

--- src/aas/site/notificationprofile/models.py
+++ src/aas/site/notificationprofile/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 from aas.site.auth.models import User
 
@@ -12,6 +13,16 @@ class NotificationProfile(models.Model):
     name = models.CharField(max_length=40)
     interval_start = models.DateTimeField()
     interval_stop = models.DateTimeField()
+
+    EMAIL = 'EM'
+    SMS = 'SM'
+    SLACK = 'SL'
+    MEDIA_CHOICES = (
+        (EMAIL, "Email"),
+        (SMS, "SMS"),
+        (SLACK, "Slack"),
+    )
+    media = MultiSelectField(choices=MEDIA_CHOICES, min_choices=1, default=EMAIL)
 
     def __str__(self):
         return f"{self.name}: {self.interval_start.time()} - {self.interval_stop.time()}"
