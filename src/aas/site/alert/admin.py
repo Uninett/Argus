467b0db2c58f05b0b5a563383e1040a9528312e0
Dabble <ddabble@users.noreply.github.com>
2019-10-29 15:59:25 +0100

    Added alert relation models :sparkles:

--- src/aas/site/alert/admin.py
+++ src/aas/site/alert/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Alert, NetworkSystem, NetworkSystemType, Object, ObjectType, ParentObject, ProblemType
+from .models import Alert, AlertRelation, AlertRelationType, NetworkSystem, NetworkSystemType, Object, ObjectType, ParentObject, ProblemType
 
 
 class NetworkSystemTypeAdmin(admin.ModelAdmin):
@@ -39,6 +39,22 @@ class AlertAdmin(admin.ModelAdmin):
     raw_id_fields = ('object', 'parent_object')
 
 
+class AlertRelationTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class AlertRelationAdmin(admin.ModelAdmin):
+    list_display = ('get_str', 'type', 'description')
+    search_fields = ('alert1__alert_id', 'alert2__alert_id')
+
+    raw_id_fields = ('alert1', 'alert2')
+
+    def get_str(self, alert_relation):
+        return str(alert_relation)
+
+    get_str.short_description = "Alert relation"
+
+
 admin.site.register(NetworkSystemType, NetworkSystemTypeAdmin)
 admin.site.register(NetworkSystem, NetworkSystemAdmin)
 admin.site.register(ObjectType, ObjectTypeAdmin)
@@ -46,3 +62,6 @@ admin.site.register(Object, ObjectAdmin)
 admin.site.register(ParentObject, ParentObjectAdmin)
 admin.site.register(ProblemType, ProblemTypeAdmin)
 admin.site.register(Alert, AlertAdmin)
+
+admin.site.register(AlertRelation, AlertRelationAdmin)
+admin.site.register(AlertRelationType, AlertRelationTypeAdmin)
