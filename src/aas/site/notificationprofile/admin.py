a3a34885ddda2fa81fb8381ef6f1b695f59d6cdd
Dabble <ddabble@users.noreply.github.com>
2019-11-07 06:06:20 +0100

    Improved notification profile models :card_file_box:
    Removed a lot of the old migrations and added a couple migrations that delete the NotificationProfile model and then re-add it together with the new models (Filter, TimeSlotGroup, TimeSlot). The NotificationProfile model was deleted to not have to provide a default value for the new required foreign key fields: `time_slot_group` and `filters`.
    
    Also:
     * Changed `start` and `end` of TimeSlot from `DateTimeField` to just `TimeField`, which easier allows the frontend to post time slots with only time and no date
     * Moved `filters` field from TimeSlotGroup to NotificationProfile
     * Renamed `Filter.filter` to `Filter.query`
     * Added `active` field to NotificationProfile

--- src/aas/site/notificationprofile/admin.py
+++ src/aas/site/notificationprofile/admin.py
@@ -1,17 +1,73 @@
 from django.contrib import admin
+from django.db.models.functions import Concat
+from django.utils.html import format_html_join
+
+from .models import Filter, NotificationProfile, TimeSlot, TimeSlotGroup
 
 
-from .models import NotificationProfile,TimeSlot, TimeSlotGroup, Filter
+class TimeSlotGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'get_time_slots')
+
+    raw_id_fields = ('user',)
+
+    def get_time_slots(self, time_slot_group):
+        return format_html_join(
+            "", "<div>{}</div>",
+            ((ts,) for ts in time_slot_group.time_slots.all())
+        )
+
+    get_time_slots.short_description = "Time slots"
 
 
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('get_str', 'day', 'start', 'end')
+    list_filter = ('day',)
+    search_fields = ('group__user', 'group__name',)
+    ordering = [Concat('group__user', 'group__name')]
 
-class NotificationProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'media')
+    raw_id_fields = ('group',)
+
+    def get_str(self, time_slot):
+        return f"[{time_slot.group.user}] {time_slot.group}"
+
+    get_str.short_description = "[User] Time slot group"
+    get_str.admin_order_field = Concat('group__user', 'group__name')
+
+
+class FilterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'query')
 
     raw_id_fields = ('user',)
 
 
-admin.site.register(TimeSlotGroup)
-admin.site.register(TimeSlot)
-admin.site.register(Filter)
+class NotificationProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_str', 'get_filters', 'get_media', 'active')
+
+    raw_id_fields = ('user', 'time_slot_group')
+    filter_horizontal = ('filters',)
+
+    def get_str(self, notification_profile):
+        return f"[{notification_profile.time_slot_group.user}] {notification_profile.time_slot_group}"
+
+    get_str.short_description = "[User] Time slot group"
+    get_str.admin_order_field = Concat('time_slot_group__user', 'time_slot_group__name')
+
+    def get_filters(self, notification_profile):
+        return format_html_join(
+            "", "<div>{}</div>",
+            ((f,) for f in notification_profile.filters.all())
+        )
+
+    get_filters.short_description = "Filters"
+
+    def get_media(self, notification_profile):
+        return notification_profile.get_media_display()
+
+    get_media.short_description = "Notification media"
+    get_media.admin_order_field = 'media'
+
+
+admin.site.register(TimeSlotGroup, TimeSlotGroupAdmin)
+admin.site.register(TimeSlot, TimeSlotAdmin)
+admin.site.register(Filter, FilterAdmin)
 admin.site.register(NotificationProfile, NotificationProfileAdmin)
--- src/aas/site/notificationprofile/models.py
+++ src/aas/site/notificationprofile/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime, time
+
 from django.db import models
+from django.utils import timezone
 from multiselectfield import MultiSelectField
 
 from aas.site.auth.models import User
@@ -6,6 +9,9 @@ from aas.site.auth.models import User
 
 class TimeSlotGroup(models.Model):
     class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'user'], name="unique_name_per_user"),
+        ]
         ordering = ['name']
 
     user = models.ForeignKey(
@@ -15,10 +21,9 @@ class TimeSlotGroup(models.Model):
     )
     name = models.CharField(max_length=40)
 
-    filters = models.ManyToManyField(Filter, related_name='notification_profiles')
-
     def __str__(self):
         return self.name
+
 
 class TimeSlot(models.Model):
     MONDAY = 'MO'
@@ -44,18 +49,46 @@ class TimeSlot(models.Model):
     )
 
     day = models.CharField(max_length=2, choices=DAY_CHOICES)
-    start = models.DateTimeField()  # TimeField?
-    end = models.DateTimeField()
+    start = models.TimeField(help_text="Local time.")
+    end = models.TimeField(help_text="Local time.")
+
+    # TODO: replace these with making `start` and `end` custom timezone aware time fields
+    #       (time+timezone is saved in database, to_python() returns custom time object that requires specifying a timezone
+    #        - for comparison of objects)
+    @property
+    def utc_start(self):
+        return self.local_time_to_utc_time(self.start)
+
+    @property
+    def utc_end(self):
+        return self.local_time_to_utc_time(self.end)
+
+    @staticmethod
+    def local_time_to_utc_time(local_time: time):
+        current_localtime = timezone.localtime()
+        local_datetime = datetime.combine(current_localtime.date(), local_time, current_localtime.tzinfo)
+        return local_datetime.astimezone(timezone.utc).time()
+
+    def __str__(self):
+        return f"{self.start}-{self.end} on {self.get_day_display()}s"
 
 
 class Filter(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'user'], name="unique_name_per_user"),
+        ]
+
     user = models.ForeignKey(
         User,
         models.CASCADE,
         related_name='filters',
     )
     name = models.CharField(max_length=40)
-    filter = models.TextField()
+    query = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} [{self.query}]"
 
 
 class NotificationProfile(models.Model):
@@ -64,8 +97,15 @@ class NotificationProfile(models.Model):
         models.CASCADE,
         related_name='notification_profiles',
     )
-    group = models.ManyToManyField(
-        TimeSlotGroup,
+    # TODO: add constraint that user must be the same
+    time_slot_group = models.OneToOneField(
+        to=TimeSlotGroup,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='notification_profile',
+    )
+    filters = models.ManyToManyField(
+        to=Filter,
         related_name='notification_profiles',
     )
 
@@ -78,7 +118,7 @@ class NotificationProfile(models.Model):
         (SLACK, "Slack"),
     )
     media = MultiSelectField(choices=MEDIA_CHOICES, min_choices=1, default=EMAIL)
+    active = models.BooleanField(default=True)
 
-    # TODO: name based on filter and time_slot_group
-    # def __str__(self):
-    #     return
+    def __str__(self):
+        return f"{self.time_slot_group}: {', '.join(str(f) for f in self.filters.all())}"
