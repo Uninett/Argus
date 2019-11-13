55e5770a5af6a4a812c2c9a3a35298ac3519967e
Tjedor <theo.aw@hotmail.com>
2019-11-13 17:01:31 +0100

    add PO for preview and metadata

--- src/aas/site/notificationprofile/tests.py
+++ src/aas/site/notificationprofile/tests.py
@@ -1,1 +1,11 @@
-# Create your tests here.
+from django.test import TestCase
+from .models import Filter
+
+class FilterTestCase(TestCase):
+    def setUp(self):
+        Filter.objects.create(name="filter1", user=1, string="dette er et filter")
+
+    def test_filter_creation(self):
+        """Animals that can speak are correctly identified"""
+        filter1 = Filter.objects.get(name="filter1")
+        self.assertEqual(filter1.string, 'dette er et filter')
