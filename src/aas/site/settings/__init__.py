9f07f4c769e86b3a0ee70f9e85a6e7b946d692d7
Hanne Moa <hanne.moa@uninett.no>
2020-04-24 13:44:11 +0200

    Get rid of get_dataporten_secret()

--- src/aas/site/settings/__init__.py
+++ src/aas/site/settings/__init__.py
@@ -1,3 +1,4 @@
+from os import getenv
 from pathlib import Path
 
 
@@ -5,9 +6,47 @@ __all__ = [
     'SETTINGS_DIR',
     'SITE_DIR',
     'BASE_DIR',
+    'get_bool_env',
+    'get_str_env',
+    'get_int_env',
 ]
+
 
 # Build paths inside the project like this: BASE_DIR / ...
 SETTINGS_DIR = Path(__file__).resolve().parent
 SITE_DIR = SETTINGS_DIR.parent
 BASE_DIR = SITE_DIR.parent
+
+
+def get_any_env(envname, required=False):
+    env = getenv(envname)
+    if env is None:
+        # Envvar not set!
+        if required:
+            raise OsError("Environment variable \"{}\" not set!", envname)
+        return None
+    return env
+
+
+def get_bool_env(envname, default=None, required=False):
+    env = get_any_env(envname, default, required)
+    env = str(env).lower()
+    if env in ("1", "on", "true", "yes"):
+        return True
+    if env in ("0", "off", "false", "no"):
+        return False
+    return default
+
+
+def get_str_env(envname, default='', required=False):
+    env = get_any_env(envname, default, required)
+    if env is None:
+        return default
+    return str(env)
+
+
+def get_int_env(envname, default=0, required=False):
+    env = get_any_env(envname, default, required)
+    if env is None:
+        return default
+    return int(env)
