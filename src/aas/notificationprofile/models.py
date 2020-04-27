ccb60041f5335acc51908c5925ff4a9a73cf1b0c
Hanne Moa <hanne.moa@uninett.no>
2020-04-27 10:32:27 +0200

    Improve constraint names
    Index and constraint names function as documentation in the database and
    should always, at a minimum, be prefixed with what table/view they are
    for. In earlier Djangos, index names were generated like this:
    <app_label>_<model_class_label>_<index_specific>. These names must be unique,
    and a prefixed "namespace" helps assure that.

--- src/aas/notificationprofile/models.py
+++ src/aas/notificationprofile/models.py
@@ -17,7 +17,7 @@ class TimeSlot(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["name", "user"], name="unique_name_per_user1"
+                fields=["name", "user"], name="timeslot_unique_name_per_user"
             ),
         ]
         ordering = ["name"]
@@ -121,7 +121,7 @@ class Filter(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["name", "user"], name="unique_name_per_user2"
+                fields=["name", "user"], name="filter_unique_name_per_user"
             ),
         ]
 
