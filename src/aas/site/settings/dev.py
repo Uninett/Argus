1d1e3d9cdaf39692923bab1ee5ab924909068432
Hanne Moa <hanne.moa@uninett.no>
2020-06-09 10:26:19 +0200

    Move site-specific settings from base settings

--- src/aas/site/settings/dev.py
+++ src/aas/site/settings/dev.py
@@ -6,7 +6,15 @@ TEMPLATES[0]["OPTIONS"]["debug"] = get_bool_env("TEMPLATE_DEBUG", True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_str_env("SECRET_KEY", "secret-secret!")
+STATIC_URL = get_str_env("STATIC_URL", "/static/")
+STATIC_ROOT = get_str_env("STATIC_ROOT", "static/")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 FRONTEND_URL = get_str_env("AAS_FRONTEND_URL", "http://localhost:3000")
 
@@ -18,3 +26,14 @@ CORS_ORIGIN_WHITELIST = [
 
 # Prints sent emails to the console
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# PSA for login
+
+SOCIAL_AUTH_DATAPORTEN_KEY = get_str_env("AAS_DATAPORTEN_KEY")
+SOCIAL_AUTH_DATAPORTEN_SECRET = get_str_env("AAS_DATAPORTEN_SECRET")
+
+SOCIAL_AUTH_DATAPORTEN_EMAIL_KEY = SOCIAL_AUTH_DATAPORTEN_KEY
+SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET = SOCIAL_AUTH_DATAPORTEN_SECRET
+
+SOCIAL_AUTH_DATAPORTEN_FEIDE_KEY = SOCIAL_AUTH_DATAPORTEN_KEY
+SOCIAL_AUTH_DATAPORTEN_FEIDE_SECRET = SOCIAL_AUTH_DATAPORTEN_SECRET
--- src/aas/site/settings/prod.py
+++ src/aas/site/settings/prod.py
@@ -6,7 +6,7 @@ TEMPLATES[0]["OPTIONS"]["debug"] = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_str_env("SECRET_KEY", required=True)
-STATIC_URL = get_str_env("STATIC_URL", "/adminstatic/")
+STATIC_URL = get_str_env("STATIC_URL", "/static/")
 STATIC_ROOT = get_str_env("STATIC_ROOT", required=True)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -28,3 +28,14 @@ EMAIL_HOST = get_str_env("EMAIL_HOST", "smtp.gmail.com", required=True)
 EMAIL_PORT = get_int_env("EMAIL_PORT", 587)
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = get_str_env("EMAIL_HOST_PASSWORD")
+
+# PSA for login
+
+SOCIAL_AUTH_DATAPORTEN_KEY = get_str_env("AAS_DATAPORTEN_KEY", required=True)
+SOCIAL_AUTH_DATAPORTEN_SECRET = get_str_env("AAS_DATAPORTEN_SECRET", required=True)
+
+SOCIAL_AUTH_DATAPORTEN_EMAIL_KEY = SOCIAL_AUTH_DATAPORTEN_KEY
+SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET = SOCIAL_AUTH_DATAPORTEN_SECRET
+
+SOCIAL_AUTH_DATAPORTEN_FEIDE_KEY = SOCIAL_AUTH_DATAPORTEN_KEY
+SOCIAL_AUTH_DATAPORTEN_FEIDE_SECRET = SOCIAL_AUTH_DATAPORTEN_SECRET
