02a67736cc8362f931b91414fcedca849bb4ac2d
Dabble <ddabble@users.noreply.github.com>
2020-07-29 15:05:20 +0200

    Move create_default_timeslot() to global scope
    Also add check on whether user already has any timeslots.

--- src/argus/notificationprofile/models.py
+++ src/argus/notificationprofile/models.py
@@ -32,19 +32,19 @@ class Timeslot(models.Model):
                 return True
         return False
 
-    # Create default immediate Timeslot when a user is created
-    @staticmethod
-    @receiver(post_save, sender=User)
-    def create_default_timeslot(sender, instance, created, raw, *args, **kwargs):
-        if raw or not created:
-            return
 
-        TimeRecurrence.objects.create(
-            timeslot=Timeslot.objects.create(user=instance, name="Immediately"),
-            days=[day for day in TimeRecurrence.Day.values],
-            start=TimeRecurrence.DAY_START,
-            end=TimeRecurrence.DAY_END,
-        )
+# Create default immediate Timeslot when a user is created
+@receiver(post_save, sender=User)
+def create_default_timeslot(sender, instance: User, created, raw, *args, **kwargs):
+    if raw or not created or instance.timeslots.exists():
+        return
+
+    TimeRecurrence.objects.create(
+        timeslot=Timeslot.objects.create(user=instance, name="Immediately"),
+        days=[day for day in TimeRecurrence.Day.values],
+        start=TimeRecurrence.DAY_START,
+        end=TimeRecurrence.DAY_END,
+    )
 
 
 class TimeRecurrence(models.Model):
