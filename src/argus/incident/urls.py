deb0613897786ade012e9c8c18470840f4b4bcb6
Dabble <ddabble@users.noreply.github.com>
2020-07-31 15:37:34 +0200

    Rename URLs from alert to incident

--- src/argus/incident/urls.py
+++ src/argus/incident/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+
 incident_list = views.IncidentViewSet.as_view({"get": "list", "post": "create"})
 incident_detail = views.IncidentViewSet.as_view({"get": "retrieve",})
 incident_active = views.IncidentViewSet.as_view({"put": "active",})
@@ -9,10 +10,10 @@ incident_ticket_url = views.IncidentViewSet.as_view({"put": "ticket_url",})
 
 app_name = "incident"
 urlpatterns = [
-    path("", incident_list, name="alerts"),
-    path("<int:pk>", incident_detail, name="alert"),
-    path("<int:pk>/active", incident_active, name="alert-active"),
-    path("<int:pk>/ticket_url", incident_ticket_url, name="alert-ticket-url"),
+    path("", incident_list, name="incidents"),
+    path("<int:pk>", incident_detail, name="incident"),
+    path("<int:pk>/active", incident_active, name="incident-active"),
+    path("<int:pk>/ticket_url", incident_ticket_url, name="incident-ticket-url"),
     path("active/", views.ActiveIncidentList.as_view(), name="incidents-active"),
     path("source-types/", views.SourceSystemTypeList.as_view(), name="source-types"),
     path("sources/", views.SourceSystemList.as_view(), name="sources"),
