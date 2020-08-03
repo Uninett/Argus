ef5c6d4911590124e2d1c5d2d83ae52d24ec01ea
Hanne Moa <hanne.moa@uninett.no>
2020-08-03 12:53:29 +0200

    Add logging setup useful for dev

--- src/argus/site/settings/dev.py
+++ src/argus/site/settings/dev.py
@@ -1,8 +1,12 @@
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from .base import *
+
 
 
 DEBUG = get_bool_env("DEBUG", True)
@@ -49,3 +53,48 @@ SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET = SOCIAL_AUTH_DATAPORTEN_SECRET
 
 SOCIAL_AUTH_DATAPORTEN_FEIDE_KEY = SOCIAL_AUTH_DATAPORTEN_KEY
 SOCIAL_AUTH_DATAPORTEN_FEIDE_SECRET = SOCIAL_AUTH_DATAPORTEN_SECRET
+
+## Logging setup
+
+LOGGING_CONFIG = None
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+    },
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+    },
+    'loggers': {
+        # root logger
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console',],
+        },
+        'django.db.backends': {
+            'handlers': ['null'],
+            'level': 'NOTSET',
+            'propagate': False,
+        },
+        'django.utils.autoreload': {
+            'handlers': ['null'],
+            'level': 'NOTSET',
+            'propagate': False,
+        },
+        #'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+    },
+})
