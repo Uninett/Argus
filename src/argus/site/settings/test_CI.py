5e1231b63ce118b26ceef6e9f2462875ec697b6e
Dabble <ddabble@users.noreply.github.com>
2020-08-04 19:14:49 +0200

    Add running tests with PostgreSQL to GitHub action

--- src/argus/site/settings/test_CI.py
+++ src/argus/site/settings/test_CI.py
@@ -1,1 +1,24 @@
 from .dev import *
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+POSTGRES = get_bool_env("POSTGRES")
+POSTGRES_DB = get_str_env("POSTGRES_DB")
+POSTGRES_USER = get_str_env("POSTGRES_USER")
+POSTGRES_PASSWORD = get_str_env("POSTGRES_PASSWORD")
+
+if POSTGRES:
+    # fmt: off
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": "localhost",
+        }
+    }
+    # fmt: on
+    _LOGGER.debug(f"Using PostgreSQL as database backend.")
