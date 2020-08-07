bbf1c0585cdb2b7474b72673f4a29a05664d1c3d
Dabble <ddabble@users.noreply.github.com>
2020-08-07 14:51:42 +0200

    Add tests for active+inactive incident endpoints

--- src/argus/incident/tests/test_incident.py
+++ src/argus/incident/tests/test_incident.py
@@ -2,10 +2,13 @@ from datetime import datetime, timedelta
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.timezone import is_aware, make_aware
+from rest_framework.test import APIClient, APITestCase
 
 from argus.auth.models import User
+from argus.site import datetime_utils
 from argus.site.utils import duplicate
 from ..models import Incident, Object, ObjectType, ProblemType, SourceSystem, SourceSystemType
 
@@ -112,3 +115,75 @@ class EndTimeInfinityFieldTests(TestCase):
 
         self.assertFalse(Incident.objects.filter(end_time__lt="-infinity").exists())
         self.assertFalse(Incident.objects.filter(end_time__gt="infinity").exists())
+
+
+class IncidentAPITests(APITestCase):
+    def setUp(self):
+        # TODO: reduce code duplication after Incident model is more finalized
+        source_system_type = SourceSystemType.objects.create(name="Type")
+        source_system_user = User.objects.create_user(username="system_1")
+        source_system1 = SourceSystem.objects.create(name="System 1", type=source_system_type, user=source_system_user)
+        object_type = ObjectType.objects.create(name="ObjectType")
+        object1 = Object.objects.create(name="Object 1", object_id="123", type=object_type)
+        problem_type = ProblemType.objects.create(name="ProblemType")
+
+        self.stateful_incident1 = Incident.objects.create(
+            start_time=make_aware(datetime(2000, 1, 1)),
+            end_time=timezone.now(),
+            source=source_system1,
+            source_incident_id="1",
+            object=object1,
+            problem_type=problem_type,
+        )
+        self.stateless_incident1 = duplicate(self.stateful_incident1, end_time=None, source_incident_id="2")
+
+        password = "1234"
+        self.user1 = User.objects.create_user(username="user1", password=password, is_staff=True, is_superuser=True)
+
+        self.rest_client = APIClient()
+        self.rest_client.force_authenticate(user=self.user1)
+
+        self.active_url = lambda incident: reverse("incident:incident-active", args=[incident.pk])
+        self.inactive_url = lambda incident: reverse("incident:incident-inactive", args=[incident.pk])
+
+    def test_active_endpoints_properly_change_stateful_incidents(self):
+        self.assertTrue(self.stateful_incident1.stateful)
+        self.assertFalse(self.stateful_incident1.active)
+
+        def assert_incident_active_and_end_time_is_infinity():
+            self.rest_client.put(self.active_url(self.stateful_incident1))
+            self.stateful_incident1.refresh_from_db()
+            self.assertTrue(self.stateful_incident1.active)
+            self.assertEqual(datetime_utils.make_naive(self.stateful_incident1.end_time), datetime.max)
+
+        assert_incident_active_and_end_time_is_infinity()
+        # Test that endpoint is idempotent
+        assert_incident_active_and_end_time_is_infinity()
+
+        test_start_time = timezone.now()
+        self.rest_client.put(self.inactive_url(self.stateful_incident1))
+        self.stateful_incident1.refresh_from_db()
+        self.assertFalse(self.stateful_incident1.active)
+        set_end_time = self.stateful_incident1.end_time
+        self.assertGreater(set_end_time, test_start_time)
+        self.assertLess(set_end_time, timezone.now())
+
+        # Test that endpoint is idempotent
+        self.rest_client.put(self.inactive_url(self.stateful_incident1))
+        self.stateful_incident1.refresh_from_db()
+        self.assertEqual(self.stateful_incident1.end_time, set_end_time)
+
+    def test_active_endpoints_do_not_change_stateless_incidents(self):
+        def assert_incident_stateless():
+            self.stateless_incident1.refresh_from_db()
+            self.assertFalse(self.stateless_incident1.stateful)
+            self.assertFalse(self.stateless_incident1.active)
+
+        assert_incident_stateless()
+        response = self.rest_client.put(self.active_url(self.stateless_incident1))
+        self.assertEqual(response.status_code, 400)
+        assert_incident_stateless()
+
+        response = self.rest_client.put(self.inactive_url(self.stateless_incident1))
+        self.assertEqual(response.status_code, 400)
+        assert_incident_stateless()
