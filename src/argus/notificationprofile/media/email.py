7f7fd22767e46beb2332c697dadeca2c9984863d
Hanne Moa <hanne.moa@uninett.no>
2020-08-07 11:17:32 +0200

    Break out exception-handling for email notifications

--- src/argus/notificationprofile/media/email.py
+++ src/argus/notificationprofile/media/email.py
@@ -1,13 +1,10 @@
 import json
 import logging
-from abc import ABC, abstractmethod
 
 from django.conf import settings
 from django.template.loader import render_to_string
 from rest_framework.renderers import JSONRenderer
 
-from argus.auth.models import User
-from argus.incident.models import Incident
 from argus.incident.serializers import IncidentSerializer
 
 from .base import NotificationMedium
@@ -16,6 +13,24 @@ from .base import NotificationMedium
 LOG = logging.getLogger(__name__)
 
 __all__ = ['EmailNotification']
+
+
+def send_email_safely(function, additional_error=None, *args, **kwargs):
+    try:
+        result = function(*args, **kwargs)
+        return result
+    except ConnectionRefusedError as e:
+        EMAIL_HOST = getattr(settings, "EMAIL_HOST", None)
+        if not EMAIL_HOST:
+            LOG.error("Notification: Email: EMAIL_HOST not set, cannot send")
+        EMAIL_PORT = getattr(settings, "EMAIL_PORT", None)
+        if not EMAIL_PORT:
+            LOG.error("Notification: Email: EMAIL_PORT not set, cannot send")
+        if EMAIL_HOST and EMAIL_PORT:
+            LOG.error('Notification: Email: Connection refused to "%s", port "%s"', EMAIL_HOST, EMAIL_PORT)
+        if additional_error:
+            LOG.error(*additional_error)
+        # TODO: Store error as incident
 
 
 class EmailNotification(NotificationMedium):
@@ -36,19 +51,7 @@ class EmailNotification(NotificationMedium):
             "incident_dict": incident_dict,
             "longest_field_name_length": len(max(incident_dict, key=len)),
         }
-        try:
-            user.email_user(
-                subject=f"{settings.NOTIFICATION_SUBJECT_PREFIX}{title}",
-                message=render_to_string("notificationprofile/email.txt", template_context),
-                html_message=render_to_string("notificationprofile/email.html", template_context),
-            )
-        except ConnectionRefusedError as e:
-            EMAIL_HOST = getattr(settings, "EMAIL_HOST", None)
-            if not EMAIL_HOST:
-                LOG.error("Notification: Email: EMAIL_HOST not set, cannot send")
-            EMAIL_PORT = getattr(settings, "EMAIL_PORT", None)
-            if not EMAIL_PORT:
-                LOG.error("Notification: Email: EMAIL_PORT not set, cannot send")
-            if EMAIL_HOST and EMAIL_PORT:
-                LOG.error('Notification: Email: Connection refused to "%s", port "%s"', EMAIL_HOST, EMAIL_PORT)
-            # TODO: Store error as incident
+        subject = f"{settings.NOTIFICATION_SUBJECT_PREFIX}{title}"
+        message = render_to_string("notificationprofile/email.txt", template_context)
+        html_message = render_to_string("notificationprofile/email.html", template_context)
+        send_email_safely(user.email_user, subject=subject, message=message, html_message=html_message)
