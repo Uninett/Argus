dbc316158db07510eb772ace53de795e1a959937
Dabble <ddabble@users.noreply.github.com>
2020-08-19 14:08:23 +0200

    Add User "type" convenience properties

--- src/argus/auth/models.py
+++ src/argus/auth/models.py
@@ -5,7 +5,13 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    pass
+    @property
+    def is_source_system(self):
+        return hasattr(self, "source_system")
+
+    @property
+    def is_end_user(self):
+        return not hasattr(self, "source_system")
 
 
 class PhoneNumber(models.Model):
