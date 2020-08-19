09ae2b816a03f845f26f2f3b4e21f511afc5de14
Dabble <ddabble@users.noreply.github.com>
2020-08-19 03:55:45 +0200

    Add auth token expiration

--- src/argus/auth/tests.py
+++ src/argus/auth/tests.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -27,6 +30,12 @@ class APITests(APITestCase):
     def assemble_token_auth_kwarg(token_key: str):
         return {"HTTP_AUTHORIZATION": f"Token {token_key}"}
 
+    @staticmethod
+    def expire_token(token: Token):
+        # Subtract an extra second, just to be sure
+        token.created -= timedelta(days=settings.AUTH_TOKEN_EXPIRES_AFTER_DAYS, seconds=1)
+        token.save()
+
     def test_logout_deletes_token(self):
         logout_path = reverse("auth:logout")
 
@@ -41,6 +50,56 @@ class APITests(APITestCase):
 
         assert_token_is_deleted(self.normal_user1_token, self.normal_user1, self.normal_user1_client)
         assert_token_is_deleted(self.superuser1_token, self.superuser1, self.superuser1_client)
+
+    def _successfully_get_auth_token(self, user: User, user_password: str, client: APIClient):
+        auth_token_path = reverse("api-token-auth")
+        response = client.post(auth_token_path, {"username": user.username, "password": user_password})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        return response
+
+    def test_auth_token_expires_and_is_deleted(self):
+        some_auth_required_path = reverse("auth:current-user")
+
+        def assert_token_expires_and_is_deleted(user: User, token: Token, client: APIClient):
+            self.assertEqual(client.get(some_auth_required_path).status_code, status.HTTP_200_OK)
+
+            self.expire_token(token)
+
+            self.assertEqual(client.get(some_auth_required_path).status_code, status.HTTP_401_UNAUTHORIZED)
+            user.refresh_from_db()
+            self.assertFalse(hasattr(user, "auth_token"))
+            with self.assertRaises(Token.DoesNotExist):
+                token.refresh_from_db()
+
+        assert_token_expires_and_is_deleted(self.normal_user1, self.normal_user1_token, self.normal_user1_client)
+        assert_token_expires_and_is_deleted(self.superuser1, self.superuser1_token, self.superuser1_client)
+
+    def test_can_get_auth_token_after_deletion_or_expiration(self):
+        logout_path = reverse("auth:logout")
+        some_auth_required_path = reverse("auth:current-user")
+
+        def assert_unauthorized_until_getting_auth_token(user: User, user_password: str, client: APIClient):
+            self.assertEqual(client.get(some_auth_required_path).status_code, status.HTTP_401_UNAUTHORIZED)
+
+            client.credentials()  # clears credentials
+            response = self._successfully_get_auth_token(user, user_password, client)
+            client.credentials(**self.assemble_token_auth_kwarg(response.data["token"]))
+
+            self.assertEqual(client.get(some_auth_required_path).status_code, status.HTTP_200_OK)
+
+        def assert_can_get_auth_token_after_deletion_and_expiration(user: User, user_password: str, client: APIClient):
+            client.post(logout_path)
+            assert_unauthorized_until_getting_auth_token(user, user_password, client)
+
+            self.expire_token(Token.objects.get(user=user))
+            assert_unauthorized_until_getting_auth_token(user, user_password, client)
+
+        assert_can_get_auth_token_after_deletion_and_expiration(
+            self.normal_user1, self.normal_user1_password, self.normal_user1_client
+        )
+        assert_can_get_auth_token_after_deletion_and_expiration(
+            self.superuser1, self.superuser1_password, self.superuser1_client
+        )
 
     def test_get_current_user_returns_correct_user(self):
         current_user_path = reverse("auth:current-user")
