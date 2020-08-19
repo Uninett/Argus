034fcb7783a540b62c2b08cbb973106336a02c19
Dabble <ddabble@users.noreply.github.com>
2020-08-19 14:06:23 +0200

    Move Incident setters after properties

--- src/argus/incident/models.py
+++ src/argus/incident/models.py
@@ -248,26 +248,6 @@ class Incident(models.Model):
     def open(self):
         return self.stateful and self.end_time > timezone.now()
 
-    def set_open(self, actor: User):
-        if not self.stateful:
-            raise ValidationError("Cannot set a stateless incident as open")
-        if self.open:
-            return
-
-        self.end_time = INFINITY_REPR
-        self.save(update_fields=["end_time"])
-        Event.objects.create(incident=self, actor=actor, timestamp=timezone.now(), type=Event.Type.REOPEN)
-
-    def set_closed(self, actor: User):
-        if not self.stateful:
-            raise ValidationError("Cannot set a stateless incident as closed")
-        if not self.open:
-            return
-
-        self.end_time = timezone.now()
-        self.save(update_fields=["end_time"])
-        Event.objects.create(incident=self, actor=actor, timestamp=self.end_time, type=Event.Type.CLOSE)
-
     @property
     def tags(self):
         return Tag.objects.filter(incident_tag_relations__incident=self)
@@ -297,6 +277,26 @@ class Incident(models.Model):
         acks_query = Q(ack__isnull=False)
         acks_not_expired_query = Q(ack__expiration__isnull=True) | Q(ack__expiration__gt=timezone.now())
         return self.events.filter(acks_query & acks_not_expired_query).exists()
+
+    def set_open(self, actor: User):
+        if not self.stateful:
+            raise ValidationError("Cannot set a stateless incident as open")
+        if self.open:
+            return
+
+        self.end_time = INFINITY_REPR
+        self.save(update_fields=["end_time"])
+        Event.objects.create(incident=self, actor=actor, timestamp=timezone.now(), type=Event.Type.REOPEN)
+
+    def set_closed(self, actor: User):
+        if not self.stateful:
+            raise ValidationError("Cannot set a stateless incident as closed")
+        if not self.open:
+            return
+
+        self.end_time = timezone.now()
+        self.save(update_fields=["end_time"])
+        Event.objects.create(incident=self, actor=actor, timestamp=self.end_time, type=Event.Type.CLOSE)
 
 
 @receiver(post_save, sender=Incident)
