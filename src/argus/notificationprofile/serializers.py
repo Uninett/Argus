Uninett/Argus

commit 3dd4b7784ee19edb3e738011fed4a5799c44b563

Remove superfluous "pk" read-only serializer fields

src/argus/notificationprofile/serializers.py
@@ -46,7 +46,6 @@ class TimeslotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timeslot
         fields = ["pk", "name", "time_recurrences"]
-        read_only_fields = ["pk"]
 
     def create(self, validated_data):
         time_recurrences_data = validated_data.pop("time_recurrences")
@@ -86,7 +85,6 @@ class FilterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Filter
         fields = ["pk", "name", "filter_string"]
-        read_only_fields = ["pk"]
 
 
 class NotificationProfileSerializer(serializers.ModelSerializer):
@@ -98,7 +96,6 @@ class NotificationProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotificationProfile
         fields = ["pk", "timeslot", "filters", "media", "phone_number", "active"]
-        read_only_fields = ["pk"]
 
     def create(self, validated_data):
         try:

src/argus/incident/serializers.py
@@ -35,7 +35,7 @@ class SourceSystemSerializer(RemovableFieldSerializer):
     class Meta:
         model = SourceSystem
         fields = ["pk", "name", "type", "user"]
-        read_only_fields = ["pk", "type", "user"]
+        read_only_fields = ["type", "user"]
 
 
 class IncidentTagRelationSerializer(RemovableFieldSerializer):
@@ -96,7 +96,6 @@ class IncidentSerializer(RemovableFieldSerializer):
             "description",
             "ticket_url",
         ]
-        read_only_fields = ["pk"]
 
     def create(self, validated_data: dict):
         assert "user" in validated_data
@@ -220,4 +219,3 @@ class IncidentSerializer_legacy(RemovableFieldSerializer):
             "description",
             "ticket_url",
         ]
-        read_only_fields = ["pk"]

commit 32dbff8c090372b0614d41d901b6e4ff1e60ad0c

Fix missing PK in read_only_fields
This regression was introduced in 3dd4b7784ee19edb3e738011fed4a5799c44b563.

src/argus/notificationprofile/serializers.py
@@ -96,6 +96,8 @@ class NotificationProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotificationProfile
         fields = ["pk", "timeslot", "filters", "media", "phone_number", "active"]
+        # "pk" needs to be listed, as "timeslot" is the actual primary key
+        read_only_fields = ["pk"]
 
     def create(self, validated_data):
         try: