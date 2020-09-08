55de60e492eeee7a6c57e24b89f51bf6df828690
Hanne Moa <hanne.moa@uninett.no>
2020-09-08 09:38:56 +0200

    Move CORS middleware

--- src/argus/site/settings/base.py
+++ src/argus/site/settings/base.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
