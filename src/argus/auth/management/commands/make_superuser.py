fb93122aeae8382a002a9c44a3580825fbd6d636
Hanne Moa <hanne.moa@uninett.no>
2020-10-14 13:12:42 +0200

    Add commands for user management

---
+++ src/argus/auth/management/commands/make_superuser.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from argus.auth.models import User
+
+
+class Command(BaseCommand):
+    help = "Turn user into superuser"
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str,
+                            help="Turn this user into a superuser")
+
+    def handle(self, *args, **options):
+        username = options['username']
+        user = User.objects.get(username=username)
+        self.stdout.write(f'Got user with id {user.id} for username {username}')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
