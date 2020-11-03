b1f0a804f5982a68351cc08fd06bf86e4e7c5c4f
Hanne Moa <hanne.moa@uninett.no>
2020-11-03 13:59:48 +0100

    Add command for creating sources via the CLI

---
+++ src/argus/dev/management/commands/create_source.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from argus.incident.factories import SourceSystemFactory, SourceSystemTypeFactory
+
+
+class Command(BaseCommand):
+    help = "Create new source, mainly for live testing"
+
+    def add_arguments(self, parser):
+        parser.add_argument("source", type=str, help="Create a source with name <source>, default type: argus")
+        parser.add_argument("-t", "--source-type", type=str, help="Create/use the source type <source-type> instead of argus")
+
+    def handle(self, *args, **options):
+        source = options["source"]
+        source_type = options.get("source_type") or "argus"
+        sst = SourceSystemTypeFactory(name=source_type)
+        ss = SourceSystemFactory(name=source, type=sst)
