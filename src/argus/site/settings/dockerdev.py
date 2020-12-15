ae0d7d2a7f7e3097bef1ff75b69f4f0cec40b6e9
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-12-15 12:54:29 +0100

    Use env to config docker-compose redis server

--- src/argus/site/settings/dockerdev.py
+++ src/argus/site/settings/dockerdev.py
@@ -3,15 +3,3 @@ from .dev import *
 
 # Allow all hosts, since all requests will typically come from outside the container
 ALLOWED_HOSTS = ["*"]
-
-# Redis server for channels, expected to run as a service in the docker-compose setup
-# fmt: off
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis", 6379)],
-        },
-    },
-}
-# fmt: on
