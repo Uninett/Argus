a690c398d6c949dd8ca24d0ff7fa5056af77cc74
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-12-17 10:55:18 +0100

    Stop hardcoding settings module in ASGI app
    Because:
    - This is a BUG, settings should always be selected through the
      environment

--- src/argus/ws/asgi.py
+++ src/argus/ws/asgi.py
@@ -5,12 +5,8 @@ defined in the ASGI_APPLICATION setting.
 Source:
 https://channels.readthedocs.io/en/latest/deploying.html#run-protocol-servers
 """
-
-import os
-
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "argus.site.settings.dev")
 django.setup()
 application = get_default_application()
