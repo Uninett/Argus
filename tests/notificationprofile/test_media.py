c0024297cdac9cb6a3321983679cc24af8c9afa6
Hanne Moa <hanne.moa@uninett.no>
2021-01-13 08:57:45 +0100

    fixup! Fix sending notifications to multiple profiles

--- tests/notificationprofile/test_media.py
+++ tests/notificationprofile/test_media.py
@@ -5,7 +5,7 @@ from argus.notificationprofile.media.email import modelinstance_to_dict
 
 
 class SerializeModelTest(TestCase):
-    def test_modelinstance_to_dict_should_not_change_model(self):
+    def test_modelinstance_to_dict_should_not_change_modelinstance(self):
         instance = TimeslotFactory()
         attributes1 = vars(instance)
         modelinstance_to_dict(instance)
