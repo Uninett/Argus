f54f3faeac420bb6eb7b06df2fc5299894ffa83a
Hanne Moa <hmpf@users.noreply.github.com>
2021-01-29 10:20:34 +0100

    Add back path to missing endpoint

--- src/argus/incident/urls.py
+++ src/argus/incident/urls.py
@@ -10,6 +10,7 @@ router.register(r"sources", views.SourceSystemViewSet)
 router.register(r"source-types", views.SourceSystemTypeViewSet)
 router.register(r"", views.IncidentViewSet)
 
+sourced_incident_list = views.SourceLockedIncidentViewSet.as_view({"get": "list", "post": "create"})
 
 event_list = views.EventViewSet.as_view({"get": "list", "post": "create"})
 event_detail = views.EventViewSet.as_view({"get": "retrieve"})
@@ -20,6 +21,7 @@ ack_detail = views.AcknowledgementViewSet.as_view({"get": "retrieve"})
 
 app_name = "incident"
 urlpatterns = [
+    path("mine/", sourced_incident_list, name="source_locked_incidents"),
     path("<int:incident_pk>/events/", event_list, name="incident-events"),
     path("<int:incident_pk>/events/<int:pk>/", event_detail, name="incident-event"),
     path("<int:incident_pk>/acks/", ack_list, name="incident-acks"),
