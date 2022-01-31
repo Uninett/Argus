628dbc1323fcea48a4604feb61992586d602e857
Johanna England <johanna.england@sikt.no>
2022-01-31 15:09:17 +0100

    Sync default email destination with user email

--- src/argus/notificationprofile/apps.py
+++ src/argus/notificationprofile/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-from django.conf import settings
+from django.contrib.auth.signals import user_logged_in
 from django.core.checks import register
 from django.db.models.signals import post_save, pre_save
 
@@ -10,9 +10,13 @@ class NotificationprofileConfig(AppConfig):
 
     def ready(self):
         # Signals
-        from .signals import create_default_timeslot
+        from .signals import (
+            create_default_timeslot,
+            sync_email_destination,
+        )
 
         post_save.connect(create_default_timeslot, "argus_auth.User")
+        post_save.connect(sync_email_destination, "argus_auth.User")
 
         # Settings validation
         from .checks import fallback_filter_check
--- src/argus/notificationprofile/signals.py
+++ src/argus/notificationprofile/signals.py
@@ -3,6 +3,7 @@ from .models import DestinationConfig, TimeRecurrence, Timeslot
 
 __all__ = [
     "create_default_timeslot",
+    "sync_email_destination",
 ]
 
 
@@ -17,3 +18,37 @@ def create_default_timeslot(sender, instance: User, created, raw, *args, **kwarg
         start=TimeRecurrence.DAY_START,
         end=TimeRecurrence.DAY_END,
     )
+
+
+# Sync user.email to DestinationConfig
+def sync_email_destination(sender, instance: User, created, raw, *args, **kwargs):
+    if raw or (not instance.email and not instance.destinations.exists()):
+        return
+
+    email_destinations = instance.destinations.filter(media_id="email")
+    found_synced_address = None
+    changed_destinations = []
+    for destination in email_destinations:
+        settings = destination.settings.copy()
+        if settings["email_address"] == instance.email:
+            found_synced_address = True
+            settings["synced"] = True
+        else:
+            settings["synced"] = False
+        if settings != destination.settings:
+            destination.settings = settings
+            changed_destinations.append(destination)
+    if not found_synced_address:  # Add new DestinationConfig
+        new_destination = DestinationConfig(
+            user=instance,
+            media_id="email",
+            settings={"email_address": instance.email, "synced": True},
+        )
+        # Triggers no signals
+        DestinationConfig.objects.bulk_create([new_destination])
+    if changed_destinations:  # Single query, triggers no signals
+        DestinationConfig.objects.bulk_update(objs=changed_destinations, fields=["settings"])
+    if not instance.email:
+        current_default = email_destinations.filter(settings__synced=True).first()
+        current_default.settings["synced"] = False
+        DestinationConfig.objects.bulk_update(objs=[current_default], fields=["settings"])
