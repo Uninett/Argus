bac57ff6c467ca92e85e3b4b2d504a94e5ddff01
Johanna England <johanna.england@sikt.no>
2022-02-24 15:51:23 +0100

    Replace phone_number/media_v1 with destinations

--- src/argus/auth/admin.py
+++ src/argus/auth/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import PhoneNumber, User
+from .models import User
 
-
-class PhoneNumberAdmin(admin.ModelAdmin):
-    list_display = ["user", "phone_number"]
-    search_fields = ["user", "phone_number"]
-    autocomplete_fields = ["user"]
-
-
-admin.site.register(PhoneNumber, PhoneNumberAdmin)
 admin.site.register(User, UserAdmin)
