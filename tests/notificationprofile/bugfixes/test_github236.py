e671f7eb49ec9dc8ceac2f71c2c3e19a15650d59
Johanna England <johanna.england@sikt.no>
2022-02-24 15:51:46 +0100

    Add and amend tests for notifications

--- tests/notificationprofile/bugfixes/test_github236.py
+++ tests/notificationprofile/bugfixes/test_github236.py
@@ -4,15 +4,9 @@ import json
 
 from argus.auth.factories import PersonUserFactory
 from argus.incident.models import create_fake_incident, get_or_create_default_instances
-from argus.notificationprofile.factories import (
-    TimeslotFactory,
-    NotificationProfileFactory,
-    FilterFactory,
-    MaximalTimeRecurrenceFactory,
-    MinimalTimeRecurrenceFactory,
-)
+from argus.notificationprofile import factories
 from argus.notificationprofile.media import send_notifications_to_users
-from argus.notificationprofile.models import Timeslot, NotificationProfile, Filter
+from argus.notificationprofile.models import DestinationConfig
 from argus.util.testing import disconnect_signals, connect_signals
 
 
@@ -28,22 +22,28 @@ class SendingNotificationTest(TestCase):
 
         # Create two separate timeslots
         user = PersonUserFactory()
-        timeslot1 = TimeslotFactory(user=user)
-        MaximalTimeRecurrenceFactory(timeslot=timeslot1)
-        timeslot2 = TimeslotFactory(user=user)
-        MinimalTimeRecurrenceFactory(timeslot=timeslot2)
+        timeslot1 = factories.TimeslotFactory(user=user)
+        factories.MaximalTimeRecurrenceFactory(timeslot=timeslot1)
+        timeslot2 = factories.TimeslotFactory(user=user)
+        factories.MinimalTimeRecurrenceFactory(timeslot=timeslot2)
 
         # Create a filter that matches your test incident
         (_, _, argus_source) = get_or_create_default_instances()
         filter_dict = {"sourceSystemIds": [argus_source.id], "tags": []}
         filter_string = json.dumps(filter_dict)
-        filter = FilterFactory(user=user, filter_string=filter_string)
+        filter = factories.FilterFactory(user=user, filter_string=filter_string)
 
-        # Create two notification profiles that match this filter, but attached to each their timeslot
-        self.np1 = NotificationProfileFactory(user=user, timeslot=timeslot1, active=True)
+        # Get user related destinations
+        destinations = user.destinations.all()
+
+        # Create two notification profiles that match this filter,
+        # but attached to different timeslots
+        self.np1 = factories.NotificationProfileFactory(user=user, timeslot=timeslot1, active=True)
         self.np1.filters.add(filter)
-        self.np2 = NotificationProfileFactory(user=user, timeslot=timeslot2, active=True)
+        self.np1.destinations.set(destinations)
+        self.np2 = factories.NotificationProfileFactory(user=user, timeslot=timeslot2, active=True)
         self.np2.filters.add(filter)
+        self.np2.destinations.set(destinations)
 
     def tearDown(self):
         connect_signals()
---
+++ tests/notificationprofile/test_signals.py
@@ -0,0 +1,43 @@
+from django.test import tag
+
+from rest_framework.test import APITestCase
+from argus.auth.factories import PersonUserFactory
+from argus.util.testing import disconnect_signals, connect_signals
+
+
+@tag("integration")
+class SignalTests(APITestCase):
+    def setUp(self):
+        disconnect_signals()
+
+        self.user1 = PersonUserFactory()
+        self.user2 = PersonUserFactory(email="")
+
+    def teardown(self):
+        connect_signals()
+
+    def test_default_email_destination_should_be_created_if_user_has_email(self):
+        default_destination = self.user1.destinations.first()
+        self.assertTrue(default_destination)
+        self.assertTrue(default_destination.settings["synced"])
+
+    def test_default_email_destination_should_not_be_created_if_user_has_no_email(self):
+        self.assertFalse(self.user2.destinations.first())
+
+    def test_default_email_destination_should_be_added_if_email_is_added_to_user(self):
+        self.user2.email = self.user2.username
+        self.user2.save(update_fields=["email"])
+        default_destination = self.user2.destinations.first()
+        self.assertTrue(default_destination)
+        self.assertTrue(default_destination.settings["synced"])
+
+    def test_default_email_destination_should_be_updated_if_user_email_changes(self):
+        self.user2.email = "new.email@example.com"
+        self.user2.save(update_fields=["email"])
+        default_destination = self.user2.destinations.filter(settings__synced=True).first()
+        self.assertEqual(self.user2.email, default_destination.settings["email_address"])
+
+    def test_default_email_destination_should_be_deleted_if_user_email_is_deleted(self):
+        self.user1.email = ""
+        self.user1.save(update_fields=["email"])
+        self.assertFalse(self.user1.destinations.filter(settings__synced=True))
