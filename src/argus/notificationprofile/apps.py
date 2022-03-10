2ac88ef84941c843487df0d57ab2a37c9cf4e8f5
Johanna England <johanna.england@sikt.no>
2022-03-10 12:02:05 +0100

    Sync entries of Media with MEDIA_PLUGINS

--- src/argus/notificationprofile/apps.py
+++ src/argus/notificationprofile/apps.py
@@ -2,6 +2,33 @@ from django.apps import AppConfig
 from django.contrib.auth.signals import user_logged_in
 from django.core.checks import register
 from django.db.models.signals import post_save, pre_save
+from django.db.utils import ProgrammingError
+from rest_framework.exceptions import APIException
+
+
+def sync_media():
+    from .media import MEDIA_CLASSES, MEDIA_CLASSES_DICT
+
+    try:
+        from .models import Media
+    except ImportError:
+        return
+
+    try:
+        for medium in Media.objects.all():
+            if not medium.slug in MEDIA_CLASSES_DICT.keys():
+                raise APIException("".join([medium.name, " plugin is not registered in MEDIA_PLUGINS"]))
+    except ProgrammingError:
+        return
+
+    # Check if all media plugins are also saved in Media
+    new_media = [
+        Media(slug=media_class.MEDIA_SLUG, name=media_class.MEDIA_NAME)
+        for media_class in MEDIA_CLASSES
+        if not Media.objects.filter(slug=media_class.MEDIA_SLUG)
+    ]
+    if new_media:
+        Media.objects.bulk_create(new_media)
 
 
 class NotificationprofileConfig(AppConfig):
@@ -22,3 +49,6 @@ class NotificationprofileConfig(AppConfig):
         from .checks import fallback_filter_check
 
         register(fallback_filter_check)
+
+        # Check if all media in Media has a respective class
+        sync_media()
