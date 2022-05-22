0f9fff38bd5001c2c2c90b25a998a7b7c567bc69
Johanna England <johanna.england@sikt.no>
2022-05-22 12:59:50 +0200

    Prevent deletion of user in admin

--- src/argus/auth/admin.py
+++ src/argus/auth/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 admin.site.register(User, UserAdmin)
