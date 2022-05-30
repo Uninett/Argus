426b27768a09f4d81c21b111979dd5757ea944c8
Johanna England <johanna.england@sikt.no>
2022-05-30 10:12:58 +0200

    Add endpoint to refresh token for source

--- src/argus/auth/serializers.py
+++ src/argus/auth/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import serializers
+from rest_framework import exceptions, serializers
+from rest_framework.authtoken.models import Token
 
 from .models import User
 
@@ -33,3 +34,23 @@ class UsernameSerializer(serializers.ModelSerializer):
             "username",
         ]
         read_only_fields = ["pk", "username"]
+
+
+class RefreshTokenSerializer(serializers.Serializer):
+    old_token = serializers.CharField(label=("Old token"), write_only=True)
+    token = serializers.CharField(label=("Token"), read_only=True)
+
+    def validate(self, attrs):
+        user = self.authenticate_credentials(key=attrs.get("old_token"))
+        attrs["user"] = user
+        return attrs
+
+    def authenticate_credentials(self, key):
+        try:
+            token = Token.objects.select_related("user").get(key=key)
+        except Token.DoesNotExist:
+            raise serializers.ValidationError("Incorrect token.", code="authorization")
+        if not token.user.is_active:
+            raise serializers.ValidationError("User inactive or deleted.", code="authorization")
+
+        return token.user
--- src/argus/auth/urls.py
+++ src/argus/auth/urls.py
@@ -11,6 +11,7 @@ app_name = "auth"
 urlpatterns = [
     # path("login/", django_views.LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("refresh-token/", views.RefreshTokenView.as_view(), name="refresh-token"),
     path("user/", views.CurrentUserView.as_view(), name="current-user"),
     path("users/<int:pk>/", views.BasicUserDetail.as_view(), name="user"),
 ] + router.urls
