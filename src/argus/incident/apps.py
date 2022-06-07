b658a6d371f68ece8f2dacec80ef6f689fe413e7
Johanna England <johanna.england@sikt.no>
2022-06-07 14:52:58 +0200

    Close token incident if token is updated/deleted

--- src/argus/incident/apps.py
+++ src/argus/incident/apps.py
@@ -7,9 +7,17 @@ class IncidentConfig(AppConfig):
     label = "argus_incident"
 
     def ready(self):
-        from .signals import delete_associated_user, delete_associated_event, create_first_event, send_notification
+        from .signals import (
+            close_token_incident,
+            delete_associated_user,
+            delete_associated_event,
+            create_first_event,
+            send_notification,
+        )
 
         post_delete.connect(delete_associated_user, "argus_incident.SourceSystem")
         post_delete.connect(delete_associated_event, "argus_incident.Acknowledgement")
+        post_delete.connect(close_token_incident, "authtoken.Token")
+        post_save.connect(close_token_incident, "authtoken.Token")
         post_save.connect(create_first_event, "argus_incident.Incident")
         post_save.connect(send_notification, "argus_incident.Event", dispatch_uid="send_notification")
--- src/argus/incident/signals.py
+++ src/argus/incident/signals.py
@@ -1,3 +1,8 @@
+from django.db.models import Q
+from django.utils import timezone
+from rest_framework.authtoken.models import Token
+
+from argus.incident.models import get_or_create_default_instances, Incident, Tag
 from argus.notificationprofile.media import background_send_notifications_to_users
 from .models import SourceSystem, Incident, Event, Acknowledgement
 
@@ -7,6 +12,7 @@ __all__ = [
     "create_first_event",
     "send_notification",
     "delete_associated_event",
+    "close_token_incident",
 ]
 
 
@@ -36,3 +42,31 @@ def send_notification(sender, instance: Event, *args, **kwargs):
 def delete_associated_event(sender, instance: Acknowledgement, *args, **kwargs):
     if hasattr(instance, "event") and instance.event:
         instance.event.delete()
+
+
+def close_token_incident(sender, instance: Token, *args, **kwargs):
+    if not hasattr(instance.user, "source_system"):
+        return
+
+    argus_user, _, source_system = get_or_create_default_instances()
+    tags = Tag.objects.filter(
+        (Q(key="problem_type") & Q(value="token_expiry"))
+        | (Q(key="source_system") & Q(value=instance.user.source_system))
+    )
+    open_incidents = Incident.objects.open().filter(source_id=source_system.id)
+    for tag in tags:
+        open_incidents = open_incidents.filter(incident_tag_relations__tag=tag)
+    closing_events = [
+        Event(
+            incident=incident,
+            actor=argus_user,
+            timestamp=timezone.now(),
+            type="END",
+            description=incident.description,
+        )
+        for incident in open_incidents
+    ]
+    for incident in open_incidents:
+        incident.end_time = timezone.now()
+        incident.save(update_fields=["end_time"])
+    Event.objects.bulk_create(closing_events)
