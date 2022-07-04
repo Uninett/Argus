ddcdeaf25ecd000fb08c700c3bb200a13d26bfaa
Simon Oliver Tveit <simtve@uninett.no>
2022-07-04 08:21:50 +0200

    Update test names to be more descriptive

--- tests/notificationprofile/V1/test_views.py
+++ tests/notificationprofile/V1/test_views.py
@@ -87,12 +87,12 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         response = self.user1_rest_client.get(f"/api/v1/notificationprofiles/{profile_pk}/")
         self.assertEqual(response.data["pk"], profile_pk)
 
-    def test_get_notification_profiles(self):
+    def test_get_all_notification_profiles(self):
         response = self.user1_rest_client.get("/api/v1/notificationprofiles/")
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["pk"], self.notification_profile1.pk)
 
-    def test_post_new_notification_profile(self):
+    def test_can_create_new_notification_profile_with_correct_values(self):
         response = self.user1_rest_client.post(
             "/api/v1/notificationprofiles/",
             {
@@ -106,7 +106,7 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         self.assertEqual(response.status_code, 201)
         self.assertTrue(NotificationProfile.objects.filter(pk=response.data.get("pk")))
 
-    def test_new_notificaton_profile_has_correct_media(self):
+    def test_created_notificaton_profile_has_correct_media(self):
         response = self.user1_rest_client.post(
             "/api/v1/notificationprofiles/",
             {
@@ -120,20 +120,20 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data["media"], self.media)
 
-    def test_delete_notification_profile(self):
+    def test_can_delete_notification_profile(self):
         profile_pk = self.notification_profile1.pk
         response = self.user1_rest_client.delete(f"/api/v1/notificationprofiles/{profile_pk}/")
         self.assertEqual(response.status_code, 204)
         self.assertFalse(NotificationProfile.objects.filter(pk=profile_pk).first())
 
-    def test_get_timeslots(self):
+    def test_get_all_timeslots(self):
         response = self.user1_rest_client.get("/api/v1/notificationprofiles/timeslots/")
         default_timeslot = Timeslot.objects.filter(user=self.user1, name="All the time").first()
         timeslot_pks = set([default_timeslot.pk, self.timeslot1.pk, self.timeslot2.pk])
         response_pks = set([timeslot["pk"] for timeslot in response.data])
         self.assertEqual(timeslot_pks, response_pks)
 
-    def test_create_new_timeslot(self):
+    def test_can_create_new_timeslot_with_correct_values(self):
         response = self.user1_rest_client.post(
             "/api/v1/notificationprofiles/timeslots/",
             {
@@ -144,7 +144,7 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         self.assertEqual(response.status_code, 201)
         self.assertTrue(Timeslot.objects.filter(user=self.user1, name="test-timeslot").first())
 
-    def test_create_new_timeslot_end_before_start(self):
+    def test_can_not_create_new_timeslot_with_end_time_before_start_time(self):
         response = self.user1_rest_client.post(
             "/api/v1/notificationprofiles/timeslots/",
             {
@@ -160,7 +160,7 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         response = self.user1_rest_client.get(f"/api/v1/notificationprofiles/timeslots/{timeslot_pk}/")
         self.assertEqual(response.data["pk"], timeslot_pk)
 
-    def test_update_timeslot_name(self):
+    def test_can_update_timeslot_name(self):
         timeslot_pk = self.timeslot1.pk
         new_name = "new-test-name"
         response = self.user1_rest_client.put(
@@ -170,7 +170,7 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Timeslot.objects.filter(pk=timeslot_pk).first().name, new_name)
 
-    def test_update_timeslot_end_to_before_start(self):
+    def test_can_not_update_timeslot_end_time_to_before_start_time(self):
         timeslot_pk = self.timeslot1.pk
         response = self.user1_rest_client.put(
             f"/api/v1/notificationprofiles/timeslots/{timeslot_pk}/",
@@ -181,18 +181,18 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         )
         self.assertEqual(response.status_code, 400)
 
-    def test_delete_timeslot(self):
+    def test_can_delete_timeslot(self):
         timeslot_pk = self.timeslot1.pk
         response = self.user1_rest_client.delete(f"/api/v1/notificationprofiles/timeslots/{timeslot_pk}/")
         self.assertEqual(response.status_code, 204)
         self.assertFalse(Timeslot.objects.filter(pk=timeslot_pk).first())
 
-    def test_get_filters(self):
+    def test_get_all_filters(self):
         response = self.user1_rest_client.get("/api/v1/notificationprofiles/filters/")
         self.assertTrue(len(response.data), 1)
         self.assertEqual(response.data[0]["pk"], self.filter1.pk)
 
-    def test_create_new_filter(self):
+    def test_can_create_new_filter_with_correct_values(self):
         response = self.user1_rest_client.post(
             "/api/v1/notificationprofiles/filters/",
             {
@@ -208,7 +208,7 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         response = self.user1_rest_client.get(f"/api/v1/notificationprofiles/filters/{filter_pk}/")
         self.assertEqual(response.data["pk"], filter_pk)
 
-    def test_update_filter_name(self):
+    def test_can_update_filter_name(self):
         filter_pk = self.filter1.pk
         new_name = "new-test-name"
         response = self.user1_rest_client.put(
@@ -221,19 +221,19 @@ class ViewTests(APITestCase, IncidentAPITestCaseHelper):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Filter.objects.filter(pk=filter_pk).first().name, new_name)
 
-    def test_delete_unused_filter_is_deleted(self):
+    def test_can_delete_unused_filter(self):
         filter_pk = self.filter2.pk
         response = self.user1_rest_client.delete(f"/api/v1/notificationprofiles/filters/{filter_pk}/")
         self.assertEqual(response.status_code, 204)
         self.assertFalse(Filter.objects.filter(pk=filter_pk).first())
 
-    def test_delete_used_filter_is_not_deleted(self):
+    def test_can_not_delete_used_filter(self):
         filter_pk = self.filter1.pk
         response = self.user1_rest_client.delete(f"/api/v1/notificationprofiles/filters/{filter_pk}/")
         self.assertEqual(response.status_code, 400)
         self.assertTrue(Filter.objects.filter(pk=filter_pk).first())
 
-    def test_get_filterpreview(self):
+    def test_filterpreview_returns_correct_incidents(self):
         response = self.user1_rest_client.post(
             "/api/v1/notificationprofiles/filterpreview/",
             {"sourceSystemIds": [self.source1.pk], "tags": [str(self.tag1)]},
