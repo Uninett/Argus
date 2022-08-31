5c812325b978a5c788350c0334c65ce93a73ef73
Simon Oliver Tveit <simtve@uninett.no>
2022-08-31 14:49:25 +0200

    Base docker tests settings on test_CI

--- src/argus/site/settings/dockerdev.py
+++ src/argus/site/settings/dockerdev.py
@@ -1,5 +1,5 @@
 """Settings specific for Docker-based development environments"""
-from .dev import *
+from .test_CI import *
 
 # Allow all hosts, since all requests will typically come from outside the container
 ALLOWED_HOSTS = ["*"]
