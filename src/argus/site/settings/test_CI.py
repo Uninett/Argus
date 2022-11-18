914421d7dde50e5cbdd85142e9996d791ab4dd0a
Morten Brekkevold <morten.brekkevold@sikt.no>
2022-11-18 13:09:50 +0100

    Output test result report in single file
    Having multiple report files gets chaotic very fast.

--- src/argus/site/settings/test_CI.py
+++ src/argus/site/settings/test_CI.py
@@ -45,6 +45,7 @@ TEST_RUNNER = "xmlrunner.extra.djangotestrunner.XMLTestRunner"
 TEST_OUTPUT_VERBOSE = 2
 TEST_OUTPUT_DIR = "junit"
 
+TEST_OUTPUT_FILE_NAME = "test-results.xml"
 
 # PSA for login
 
