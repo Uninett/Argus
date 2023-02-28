1d71273acca7e168fbb268e563f93249cdc8b9df
Johanna England <johanna.england@sikt.no>
2023-02-28 15:55:49 +0100

    Fix typo

--- docker/dockersettings.py
+++ docker/dockersettings.py
@@ -8,6 +8,6 @@ ALLOWED_HOSTS = ["*"]
 # Uncomment to enable both Email and SMS-as-email notification backends,
 # leave commented out to keep just the default email backend:
 # MEDIA_PLUGINS = [
-#    "argus.notificationprofile.media.email.EmailNotificationo",
+#    "argus.notificationprofile.media.email.EmailNotification",
 #    "argus.notificationprofile.media.sms_as_email.SMSNotification",
 # ]
