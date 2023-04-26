e4d2c903600698be359dad4dabdea8b5087fd320
Simon Oliver Tveit <simtve@uninett.no>
2023-04-26 10:52:22 +0200

    Verify created incidents
    check that they persisted to database and the values are
    correct (description and tags)

--- src/argus/dev/management/commands/stresstest.py
+++ src/argus/dev/management/commands/stresstest.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from urllib.parse import urljoin
 import asyncio
+import itertools
+import requests
 
 from httpx import HTTPError, AsyncClient
 
@@ -38,12 +40,13 @@ class Command(BaseCommand):
         return {
             "start_time": datetime.now().isoformat(),
             "description": "Stresstest",
-            "tags": [],
+            "tags": [{"tag": "problem_type=stresstest"}],
         }
 
     async def post_incidents_until_end_time(self, url, end_time, token, client):
         request_counter = 0
         incident_data = self.get_incident_data()
+        created_ids = []
         while True:
             if datetime.now() >= end_time:
                 break
@@ -51,17 +54,38 @@ class Command(BaseCommand):
             response = await client.post(url, json=incident_data, headers={"Authorization": f"Token {token}"})
             try:
                 response.raise_for_status()
+                incident = response.json()
+                created_ids.append(incident["pk"])
             except HTTPError:
                 msg = f"HTTP error {response.status_code}: {response.content.decode('utf-8')}"
                 raise HTTPError(msg)
             request_counter += 1
-        return request_counter
+        return created_ids
 
     async def run_workers(self, url, end_time, token, worker_count):
         async with AsyncClient() as client:
             return await asyncio.gather(
                 *(self.post_incidents_until_end_time(url, end_time, token, client) for _ in range(worker_count))
             )
+
+    def verify_created_incidents(self, incident_ids, url, token):
+        expected_data = self.get_incident_data()
+        for id in incident_ids:
+            id_url = urljoin(url, str(id))
+            response = requests.get(id_url, headers={"Authorization": f"Token {token}"})
+            response_data = response.json()
+            self.verify_tags(response_data, expected_data)
+            self.verify_description(response_data, expected_data)
+
+    def verify_tags(self, response_data, expected_data):
+        expected_tags = set([tag["tag"] for tag in expected_data["tags"]])
+        response_data = set([tag["tag"] for tag in response_data["tags"]])
+        if expected_tags != response_data:
+            raise ValueError("Expected tags are different from actual tags")
+
+    def verify_description(self, response_data, expected_data):
+        if response_data["description"] != expected_data["description"]:
+            raise ValueError("Expected description is different from actual description")
 
     def handle(self, *args, **options):
         test_duration = options.get("seconds")
@@ -71,15 +95,23 @@ class Command(BaseCommand):
         loop = asyncio.get_event_loop()
         start_time = datetime.now()
         end_time = start_time + timedelta(seconds=test_duration)
+        self.stdout.write("Starting stresstest ...")
         try:
             result = loop.run_until_complete(self.run_workers(url, end_time, token, worker_count))
         except HTTPError as e:
             self.stderr.write(self.style.ERROR(e))
-        else:
-            total_requests = sum(result)
-            seconds_run = (datetime.now() - start_time).seconds
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Stresstest complete with no errors. {total_requests} requests were sent in {seconds_run} seconds."
-                )
+            return
+        all_created_ids = list(itertools.chain.from_iterable(result))
+        seconds_run = (datetime.now() - start_time).seconds
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Stresstest complete with no errors. {len(all_created_ids)} requests were sent in {seconds_run} seconds."
             )
+        )
+        self.stdout.write("Verifying incidents were created correctly ...")
+        try:
+            self.verify_created_incidents(all_created_ids, url, token)
+        except ValueError as e:
+            self.stderr.write(self.style.ERROR(e))
+            return
+        self.stdout.write(self.style.SUCCESS("Verification complete with no errors."))
