64cc7175c85c44df33cbcd18c6497da2fd24b2fb
Johanna England <johanna.england@sikt.no>
2024-04-19 11:01:26 +0200

    Change to correct favicon

--- src/argus/site/urls.py
+++ src/argus/site/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
@@ -33,7 +34,7 @@ psa_urls = [
 ]
 
 urlpatterns = [
-    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.svg", permanent=True)),
     # path(".error/", error),  # Only needed when testing error pages and error behavior
     path("admin/", admin.site.urls),
     path("oidc/", include(psa_urls)),
