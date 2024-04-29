9280888878bd08d599d4cbbf5ec228433f758cf6
Hanne Moa <hanne.moa@sikt.no>
2024-04-29 12:00:02 +0200

    Moved from argus-server, experimental branch

---
+++ src/argus_htmx/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HtmxFrontendConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    label = "argus_htmx"
+    name = "argus_htmx"
