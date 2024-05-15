26dac0c7de01550b155981398540a280ef17e80c
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-05-15 13:54:15 +0200

    Add polling of incidents table
    Whole contents of <tbody id="table-body"> is replaced with <tr>'s of whatever incidents are returned.
    Table update info at the tfoot is also refreshed in the same response using out-of-bound swap.

--- src/argus_htmx/incidents/views.py
+++ src/argus_htmx/incidents/views.py
@@ -108,7 +108,11 @@ def incidents_table(request: HtmxHttpRequest) -> HttpResponse:
     # requests, allowing us to skip rendering the unchanging parts of the
     # template.
     if request.htmx:
-        base_template = "htmx/incidents/responses/_incidents_table_refresh.html"
+        # HX-Trigger == HTML tag id that iniated the request
+        if request.headers.get("HX-Trigger", "") == "table":
+            base_template = "htmx/incidents/responses/_incidents_table_poll.html"
+        else:
+            base_template = "htmx/incidents/responses/_incidents_table_refresh.html"
     else:
         base_template = "htmx/incidents/_base.html"
 
