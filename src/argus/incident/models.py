cddb6be3c8f4a0dba774572bcfdc1707680cbe05
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-06-27 15:30:47 +0200

    Add acked_by helper to incident model

--- src/argus/incident/models.py
+++ src/argus/incident/models.py
@@ -506,6 +506,9 @@ class Incident(models.Model):
 
         return self.events.filter((acks_query & acks_not_expired_query) | ack_is_just_being_created).exists()
 
+    def acked_by(self, group: str):
+        return Acknowledgement.objects.filter(event__incident=self, event__actor__groups__name=group).exists()
+
     def create_first_event(self):
         """Create the correct type of first event for an incident
 
