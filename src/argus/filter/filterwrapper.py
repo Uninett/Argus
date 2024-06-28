22048b5346c814b902fd7e868979dc7dab68b1fd
Hanne Moa <hanne.moa@sikt.no>
2024-06-28 14:14:35 +0200

    Add type annotations to filterwrapper.py

--- src/argus/filter/filterwrapper.py
+++ src/argus/filter/filterwrapper.py
@@ -4,7 +4,7 @@ from datetime import datetime, time
 from functools import reduce
 import logging
 from operator import or_
-from typing import TYPE_CHECKING, Dict, Optional
+from typing import TYPE_CHECKING, Dict, Optional, Any, Tuple
 
 from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
@@ -20,6 +20,7 @@ if TYPE_CHECKING:
     from argus.incident.models import Event, Incident
 
 TriState = Optional[bool]
+FilterBlobType = Dict[str, Any]  # Validator: drf serializer, depends on API version
 
 
 LOG = logging.getLogger(__name__)
@@ -41,44 +42,44 @@ class FilterWrapper:
     INT_FILTERS = (FilterKey.MAXLEVEL,)
     FILTER_KEYS = TRINARY_FILTERS + LIST_FILTERS + INT_FILTERS
 
-    def __init__(self, filterblob):
+    def __init__(self, filterblob: FilterBlobType):
         self.fallback_filter = getattr(settings, "ARGUS_FALLBACK_FILTER", {})
         self.filter = filterblob.copy()
 
-    def _get_filter_value(self, key):
+    def _get_filter_value(self, key: str) -> Optional[Any]:
         fallback_filter = self.fallback_filter.get(key, None)
         return self.filter.get(key, fallback_filter)
 
-    def _get_filter_value_and_ignored_status(self, key):
+    def _get_filter_value_and_ignored_status(self, key: str) -> Tuple[Optional[Any], bool]:
         filter_ = self._get_filter_value(key)
         if key in self.TRINARY_FILTERS:
             return filter_, filter_ is None
         return filter_, not filter_
 
     @property
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return all(self._get_filter_value_and_ignored_status(key)[1] for key in self.FILTER_KEYS)
 
-    def _incident_fits_maxlevel(self, incident: Incident):
+    def _incident_fits_maxlevel(self, incident: Incident) -> TriState:
         filter_, ignored = self._get_filter_value_and_ignored_status(FilterKey.MAXLEVEL)
         if ignored:
             return None
         return incident.level <= filter_
 
-    def _incident_fits_source_system(self, incident: Incident):
+    def _incident_fits_source_system(self, incident: Incident) -> TriState:
         filter_, ignored = self._get_filter_value_and_ignored_status(FilterKey.SOURCE_SYSTEM_IDS)
         if ignored:
             return None
         return incident.source.id in filter_
 
-    def _incident_fits_tags(self, incident: Incident):
+    def _incident_fits_tags(self, incident: Incident) -> TriState:
         filter_, ignored = self._get_filter_value_and_ignored_status(FilterKey.TAGS)
         if ignored:
             return None
         tags = set(tag.representation for tag in incident.deprecated_tags)
         return tags.issuperset(filter_)
 
-    def _incident_fits_tristate(self, incident: Incident, tristate: str):
+    def _incident_fits_tristate(self, incident: Incident, tristate: str) -> TriState:
         if tristate not in self.TRINARY_FILTERS:
             return None
         filter_, ignored = self._get_filter_value_and_ignored_status(tristate)
@@ -89,13 +90,13 @@ class FilterWrapper:
 
     # public
 
-    def event_fits(self, event):
+    def event_fits(self, event) -> bool:
         filter_, ignored = self._get_filter_value_and_ignored_status(FilterKey.EVENT_TYPES)
         if ignored:
             return True
         return event.type in filter_
 
-    def incident_fits(self, incident: Incident):
+    def incident_fits(self, incident: Incident) -> bool:
         if self.is_empty:
             return False  # Filter is empty!
         checks = {}
@@ -117,7 +118,7 @@ class ComplexFilterWrapper:
     def __init__(self, **kwargs):
         self.profile = kwargs.pop("profile", None)
 
-    def incident_fits(self, incident: Incident):
+    def incident_fits(self, incident: Incident) -> bool:
         if not self.profile.active:
             return False
         is_selected_by_time = self.profile.timeslot.timestamp_is_within_time_recurrences(incident.start_time)
@@ -128,7 +129,7 @@ class ComplexFilterWrapper:
                 return False
         return True
 
-    def event_fits(self, event: Event):
+    def event_fits(self, event: Event) -> bool:
         if not self.profile.active:
             return False
         # return as early as possible
