fcec7620bc7666732d5467b0f3ae857866fd3b74
podliashanyk <60876078+podliashanyk@users.noreply.github.com>
2024-08-19 14:36:13 +0200

    Add filterbox to incidents list
    * Make filter function swappable
    * Keep query params when reloading incidents list through htmx
    * Use hx-include for keeping query params on reload
    * Make refresh/poll work with filter and page number together
    * Fire table reload on each input change in filterbox
    
    ---------
    
    Co-authored-by: Hanne Moa <hanne.moa@sikt.no>

--- src/argus_htmx/incidents/views.py
+++ src/argus_htmx/incidents/views.py
@@ -14,9 +14,10 @@ from django_htmx.middleware import HtmxDetails
 from argus.incident.models import Incident
 from argus.util.datetime_utils import make_aware
 
-from argus_htmx.incidents.customization import get_incident_table_columns
-
+from .customization import get_incident_table_columns
+from .utils import get_filter_function
 from .forms import AckForm
+
 
 LOG = logging.getLogger(__name__)
 
@@ -88,31 +89,39 @@ def incident_list(request: HtmxHttpRequest) -> HttpResponse:
 
     # Load incidents
     qs = prefetch_incident_daughters().order_by("-start_time")
+    total_count = qs.count()
     last_refreshed = make_aware(datetime.now())
 
+    params = dict(request.GET.items())
+
+    incident_list_filter = get_filter_function()
+    filter_form, qs = incident_list_filter(request, qs)
+    filtered_count = qs.count()
+
     # Standard Django pagination
-    page_num = request.GET.get("page", "1")
-    page = Paginator(object_list=qs, per_page=10).get_page(page_num)
+    page_num = params.pop("page", "1")
+    PAGE_SIZE = 10
+    paginator = Paginator(object_list=qs, per_page=PAGE_SIZE)
+    page = paginator.get_page(page_num)
 
     # The htmx magic - use a different, minimal base template for htmx
     # requests, allowing us to skip rendering the unchanging parts of the
     # template.
     if request.htmx:
-        # HX-Trigger == HTML tag id that initiated the request
-        if request.headers.get("HX-Trigger", "") == "table":
-            base_template = "htmx/incidents/responses/_incidents_table_poll.html"
-        else:
-            base_template = "htmx/incidents/responses/_incidents_table_refresh.html"
+        base_template = "htmx/incidents/responses/_incidents_table_refresh.html"
     else:
         base_template = "htmx/incidents/_base.html"
     context = {
         "columns": columns,
-        "count": qs.count(),
+        "filtered_count": filtered_count,
+        "count": total_count,
+        "filter_form": filter_form,
         "page_title": "Incidents",
         "base": base_template,
         "page": page,
         "last_refreshed": last_refreshed,
         "update_interval": 30,
+        "per_page": PAGE_SIZE,
     }
 
     return render(request, "htmx/incidents/incident_list.html", context=context)
