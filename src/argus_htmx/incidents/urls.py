81dcb87521fcd10954f2a73d44291d1a69c659c8
Hanne Moa <hanne.moa@sikt.no>
2024-08-28 10:27:51 +0200

    Make ack-modal in details-page submit an ack

--- src/argus_htmx/incidents/urls.py
+++ src/argus_htmx/incidents/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.incident_list, name="incident-list"),
     path("<int:pk>/", views.incident_detail, name="incident-detail"),
     path("<int:pk>/ack/", views.incident_add_ack, name="incident-add-ack"),
+    path("<int:pk>/ack-detail/", views.incident_detail_add_ack, name="incident-detail-add-ack"),
 ]
--- src/argus_htmx/incidents/views.py
+++ src/argus_htmx/incidents/views.py
@@ -1,12 +1,16 @@
+from __future__ import annotations
+
 import logging
 from typing import Optional
 from datetime import datetime
 
 from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse
 
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django_htmx.middleware import HtmxDetails
@@ -19,6 +23,7 @@ from .utils import get_filter_function
 from .forms import AckForm
 
 
+User = get_user_model()
 LOG = logging.getLogger(__name__)
 
 
@@ -50,37 +55,55 @@ def incident_row(request, pk: int):
 
 def incident_detail(request, pk: int):
     incident = get_object_or_404(Incident, id=pk)
+    action_endpoints = {
+        "ack": reverse("htmx:incident-detail-add-ack", kwargs={"pk": pk}),
+    }
     context = {
         "incident": incident,
+        "endpoints": action_endpoints,
         "page_title": str(incident),
     }
     return render(request, "htmx/incidents/incident_detail.html", context=context)
 
 
-def incident_add_ack(request, pk: int, group: Optional[str] = None):
+def _incident_add_ack(pk: int, formdata, user: User, group: Optional[str] = None):
     incident = get_object_or_404(Incident, id=pk)
     is_group_member = None
     if group:
         group = get_object_or_404(Group, name=group)
-        is_group_member = request.user.groups.filter(pk=group.pk).exists()
+        is_group_member = user.groups.filter(pk=group.pk).exists()
+    form = AckForm()
+    if formdata:
+        if group and not is_group_member:
+            raise PermissionDenied("User {request.user} is not a member of the correct group")
+        form = AckForm(formdata)
+        if form.is_valid():
+            incident.create_ack(
+                user,
+                description=form.cleaned_data["description"],
+                expiration=form.cleaned_data["expiration"],
+            )
     context = {
-        "form": AckForm,
+        "form": form,
         "incident": incident,
         "page_title": str(incident),
         'group': group,
         'is_group_member': is_group_member,
     }
-    if request.POST:
-        if group and not is_group_member:
-            raise PermissionDenied("User {request.user} is not a member of the correct group")
-        form = AckForm(request.POST)
-        if form.is_valid():
-            incident.create_ack(
-                request.user,
-                description=form.cleaned_data["description"],
-                expiration=form.cleaned_data["expiration"],
-            )
+    return incident, context
+
+
+def incident_add_ack(request, pk: int, group: Optional[str] = None):
+    formdata = request.POST or None
+    _, context = _incident_add_ack(pk, formdata, request.user, group)
     return render(request, "htmx/incidents/incident_add_ack.html", context=context)
+
+
+@require_POST
+def incident_detail_add_ack(request, pk: int, group: Optional[str] = None):
+    formdata = request.POST or None
+    _incident_add_ack(pk, formdata, request.user, group)
+    return redirect("htmx:incident-detail", pk=pk)
 
 
 @require_GET
