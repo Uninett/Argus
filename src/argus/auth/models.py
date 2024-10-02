61b8d6a011dbd7244167ff1606e8a64d0557d7c7
Hanne Moa <hanne.moa@sikt.no>
2024-10-02 13:33:29 +0200

    Add model to hold user preferences

--- src/argus/auth/models.py
+++ src/argus/auth/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -9,3 +10,19 @@ class User(AbstractUser):
     @property
     def is_end_user(self):
         return not hasattr(self, "source_system")
+
+    def create_preferences(self):
+        self.save()  # triggers post_save signal
+
+
+class Preferences(models.Model):
+    class Meta:
+        verbose_name = "User Preferences"
+        verbose_name_plural = "Users' Preferences"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferences = models.JSONField(blank=True, default=dict)
+
+    @classmethod
+    def ensure_for_user(cls, user):
+        cls.objects.get_or_create(user=user)
