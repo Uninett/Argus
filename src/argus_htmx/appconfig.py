2ac550dad1bcbf409dce5f666f75be80ad402908
Hanne Moa <hanne.moa@sikt.no>
2024-10-07 10:03:16 +0200

    Make app settings easily toggleable
    .. argus can use the (OVERRIDING|EXTRA)_APPS utilities to toggle all the
    settings for the htmx frontend.

---
+++ src/argus_htmx/appconfig.py
@@ -0,0 +1,32 @@
+from argus.site.settings._serializers import ListAppSetting
+
+
+__all__ = ["APP_SETTINGS"]
+
+_app_settings = [
+    {
+        "app_name": "argus_htmx",
+        "urls": {
+            "path": "",
+            "urlpatterns_module": "argus_htmx.urls",
+        },
+        "context_processors": [
+            "argus_htmx.context_processors.theme_via_session",
+            "argus_htmx.context_processors.datetime_format_via_session",
+        ],
+        "middleware": {
+            "argus_htmx.middleware.LoginRequiredMiddleware": "end",
+        },
+    },
+    {
+        "app_name": "django_htmx",
+        "middleware": {
+            "django_htmx.middleware.HtmxMiddleware": "end",
+        },
+    },
+    {
+        "app_name": "widget_tweaks"
+    },
+]
+
+APP_SETTINGS = ListAppSetting(_app_settings).root
