537afc40595db2c339e120ec5e1e155a3638f2d8
Hanne Moa <hanne.moa@sikt.no>
2024-10-09 15:03:19 +0200

    Clean up authentication helper functions
    * Remove unused function and its test
    * Refactor existing functions

--- src/argus/auth/utils.py
+++ src/argus/auth/utils.py
@@ -1,10 +1,19 @@
 from django.conf import settings
-from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import ModelBackend, RemoteUserBackend
 from django.utils.module_loading import import_string
 
 from rest_framework.reverse import reverse
-from social_core.backends.base import BaseAuth
+
 from social_core.backends.oauth import BaseOAuth2
+
+
+_all__ = [
+    "get_authentication_backend_classes",
+    "has_model_backend",
+    "has_remote_user_backend",
+    "get_psa_authentication_backends",
+    "get_authentication_backend_name_and_type",
+]
 
 
 def get_authentication_backend_classes():
@@ -13,19 +22,24 @@ def get_authentication_backend_classes():
     return backends
 
 
-def get_psa_authentication_names(backends=None):
+def has_model_backend(backends):
+    return ModelBackend in backends
+
+
+def has_remote_user_backend(backends):
+    return RemoteUserBackend in backends
+
+
+def get_psa_authentication_backends(backends=None):
     backends = backends if backends else get_authentication_backend_classes()
-    psa_backends = set()
-    for backend in backends:
-        if issubclass(backend, BaseAuth):
-            psa_backends.add(backend.name)
-    return sorted(psa_backends)
+    return [backend for backend in backends if issubclass(backend, BaseOAuth2)]
 
 
 def get_authentication_backend_name_and_type(request):
+    # Needed for SPA /login-methods/ API endpoint
     backends = get_authentication_backend_classes()
     data = []
-    if ModelBackend in backends:
+    if has_model_backend(backends):
         data.append(
             {
                 "type": "username_password",
@@ -40,8 +54,7 @@ def get_authentication_backend_name_and_type(request):
             "url": reverse("social:begin", kwargs={"backend": backend.name}, request=request),
             "name": backend.name,
         }
-        for backend in backends
-        if issubclass(backend, BaseOAuth2)
+        for backend in get_psa_authentication_backends(backends)
     )
 
     return data
