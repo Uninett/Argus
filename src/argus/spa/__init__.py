fb12a6c05cf916425391a8e006c893f7fad991fa
Hanne Moa <hanne.moa@sikt.no>
2024-10-18 14:05:39 +0200

    Add docstring to argus.spa

---
+++ src/argus/spa/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module and its submodules are necessary for the set up and use of the
+legacy single page application (SPA) REACT frontend which can be found at
+https://github.com/uninett/Argus-frontend
+
+See docs/reference/react-frontend.rst
+"""
