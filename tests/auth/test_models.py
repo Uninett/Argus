9a05f2658026a774c731ecd243fcf56634aca1ef
Hanne Moa <hanne.moa@sikt.no>
2024-11-07 11:29:37 +0100

    Improve tests for User.is_used()

--- tests/auth/test_models.py
+++ tests/auth/test_models.py
@@ -3,13 +3,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 
 from argus.auth.factories import PersonUserFactory, SourceUserFactory
-from argus.incident.factories import SourceSystemFactory
+from argus.incident.factories import SourceSystemFactory, StatelessIncidentFactory
 
 
 User = get_user_model()
 
 
-class UserTests(TestCase):
+class UserTypeTests(TestCase):
     def test_is_end_user(self):
         user = PersonUserFactory()
         self.assertTrue(user.is_end_user)
@@ -40,6 +40,24 @@ class UserTests(TestCase):
         group = Group.objects.create(name="foobar")
         self.assertFalse(user.is_member_of_group(group))
 
+
+class UserIsUsedTests(TestCase):
     def test_is_dormant_if_no_actions_taken(self):
         user = PersonUserFactory()
         self.assertFalse(user.is_used())
+
+    def test_is_not_dormant_if_source_of_incident(self):
+        user = SourceUserFactory()
+        source = SourceSystemFactory(user=user)
+
+        incident = StatelessIncidentFactory(source=source)
+        self.assertTrue(user.is_used())
+
+    def test_is_not_dormant_if_actor_of_event(self):
+        source_user = SourceUserFactory()
+        source = SourceSystemFactory(user=source_user)
+        incident = StatelessIncidentFactory(source=source)
+
+        user = PersonUserFactory()
+        incident.create_ack(user)
+        self.assertTrue(user.is_used())
