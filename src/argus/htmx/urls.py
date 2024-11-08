9a79675bbbf1a72349da03d2edb048c8a10a98c8
Hanne Moa <hanne.moa@sikt.no>
2024-11-08 14:07:20 +0100

    Make "notificationprofiles" singular

--- src/argus/htmx/urls.py
+++ src/argus/htmx/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from .auth import views as auth_views
 from .incidents.urls import urlpatterns as incident_urls
 from .timeslots.urls import urlpatterns as timeslot_urls
-from .notificationprofiles.urls import urlpatterns as notificationprofile_urls
+from .notificationprofile.urls import urlpatterns as notificationprofile_urls
 from .destination.urls import urlpatterns as destination_urls
 from .themes.urls import urlpatterns as theme_urls
 from .dateformat.urls import urlpatterns as dateformat_urls
