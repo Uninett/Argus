35ab332617af6fe1efe6bc464e2a9084d051bc0d
Hanne Moa <hanne.moa@sikt.no>
2024-11-11 15:22:31 +0100

    move common functionality to base class

--- src/argus/notificationprofile/media/base.py
+++ src/argus/notificationprofile/media/base.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
+from rest_framework.exceptions import ValidationError
+
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
@@ -23,6 +25,26 @@ __all__ = ["NotificationMedium"]
 
 
 class NotificationMedium(ABC):
+    """
+    Must be defined by subclasses:
+
+    Class attributes:
+
+    - MEDIA_SLUG: short string id for the medium, lowercase
+    - MEDIA_NAME: human friendly id for the medium
+    - MEDIA_SETTINGS_KEY: the field in settings that is specific for this medium
+    - MEDIA_JSON_SCHEMA: A json-schema to describe the settings field to
+      javascript, used by the API
+    - Form: a django.forms.Form that validates the contents of the
+      settings-field. MEDIA_SETTINGS_KEY must be the field-name of a required
+      field in the form.
+
+    Class methods:
+
+    - send(event, destinations): How to send the given event to the given
+      destinations of type MEDIA_SLUG.
+    """
+
     class NotDeletableError(Exception):
         """
         Custom exception class that is raised when a destination cannot be
@@ -30,35 +52,58 @@ class NotificationMedium(ABC):
         """
 
     @classmethod
-    @abstractmethod
-    def validate(cls, instance: RequestDestinationConfigSerializer, dict: dict, user: User) -> dict:
+    def validate(cls, instance: RequestDestinationConfigSerializer, dict_: dict, user: User) -> dict:
         """
         Validates the settings of destination and returns a dict with
         validated and cleaned data
         """
-        pass
+        form = cls.Form(dict_["settings"])
+        if not form.is_valid():
+            raise ValidationError(form.errors)
+        return form.cleaned_data
+
+        form = cls.clean(form)
+
+        if cls.has_duplicate(user.destinations):
+            raise ValidationError(cls.MEDIA_SETTINGS_KEY, f"{cls.MEDIA_NAME} already exists")
+
+        return form.cleaned_data
 
     @classmethod
-    @abstractmethod
+    def clean(cls, form):
+        """Can change the cleaned data of a valid form"""
+        return form
+
+    @classmethod
     def has_duplicate(cls, queryset: QuerySet, settings: dict) -> bool:
         """
         Returns True if a destination with the given settings already exists
         in the given queryset
         """
-        pass
+        key = f"settings__{cls.MEDIA_SETTINGS_KEY}"
+        value = settings[cls.MEDIA_SETTINGS_KEY]
+        return queryset.filter(media_id=cls.MEDIA_SLUG, **{key: value}).exists()
 
-    @staticmethod
-    @abstractmethod
-    def get_label(destination: DestinationConfig) -> str:
+    @classmethod
+    def get_label(cls, destination: DestinationConfig) -> str:
         """
         Returns a descriptive label for this destination.
         """
-        pass
+        return destination.settings.get(cls.MEDIA_SETTINGS_KEY)
+
+    # No querysets beyond this point!
 
     @classmethod
-    def get_relevant_addresses(cls, destinations: Iterable[DestinationConfig]) -> set[DestinationConfig]:
+    def get_relevant_destinations(cls, destinations: Iterable[DestinationConfig]) -> Set[DestinationConfig]:
         """Returns a set of addresses the message should be sent to"""
-        pass
+        destinations = [
+            destination.settings[cls.MEDIA_SETTINGS_KEY]
+            for destination in destinations
+            if destination.media_id == cls.MEDIA_SLUG
+        ]
+        return set(destinations)
+
+    get_relevant_addresses = get_relevant_destinations
 
     @classmethod
     @abstractmethod
