5f149b5069f854b91e277b4e841cd36de37f713f
Hanne Moa <hanne.moa@sikt.no>
2024-11-13 14:09:36 +0100

    Return success only on changed preference

--- src/argus/auth/utils.py
+++ src/argus/auth/utils.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import logging
 
 from django.conf import settings
@@ -63,19 +64,25 @@ def save_preference(request, data, namespace, preference):
     """
     prefs = get_preference_obj(request, namespace)
     value = prefs.get_preference(preference)
+    LOG.debug("Changing %s: currently %s", preference, value)
+
     if not data.get(preference, None):
-        messages.warning(request, f"Failed to change {preference}, not in input")
         LOG.debug("Failed to change %s, not in input: %s", preference, data)
         return False
-    old_value = value
-    LOG.debug("Changing %s: currently %s", preference, old_value)
+
     form = prefs.FORMS[preference](data)
-    if form.is_valid():
-        value = form.cleaned_data[preference]
-        prefs.save_preference(preference, value)
-        messages.success(request, f"Changed {preference}: {old_value} → {value}")
-        LOG.info("Changed %s: %s → %s", preference, old_value, value)
-    else:
-        messages.warning(request, f"Failed to change {preference}")
-        LOG.warning("Failed to change %s", preference)
+    if not form.is_valid():
+        messages.warning(request, f"Failed to change {preference}, invalid input")
+        LOG.warning("Failed to change %s, invalid input: %s", preference, data)
+        return False
+
+    old_value = deepcopy(value)  # Just in case value is mutable..
+    value = form.cleaned_data[preference]
+    if value == old_value:
+        LOG.debug("Did not change %s: no change", preference)
+        return False
+
+    prefs.save_preference(preference, value)
+    messages.success(request, f"Changed {preference}: {old_value} → {value}")
+    LOG.info("Changed %s: %s → %s", preference, old_value, value)
     return True
