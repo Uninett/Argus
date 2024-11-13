0150380bbdf4102b1b96c87b53a196489177d6dd
Hanne Moa <hanne.moa@sikt.no>
2024-11-13 12:56:23 +0100

    Cleanup theme template naming
    * Mark theme_dropdown.html as partial
    * Mark theme_list.html as partial
    * Remove unused view

--- src/argus_htmx/themes/urls.py
+++ src/argus_htmx/themes/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 app_name = "htmx"
 urlpatterns = [
-    path("", views.ThemeListView.as_view(), name="theme-list"),
     path("names/", views.theme_names, name="theme-names"),
     path("change/", views.change_theme, name="change-theme"),
 ]
--- src/argus_htmx/themes/views.py
+++ src/argus_htmx/themes/views.py
@@ -1,10 +1,9 @@
 import logging
 
 from django.shortcuts import render
-from django.views.generic import ListView
 
 from django.views.decorators.http import require_GET, require_POST
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django_htmx.http import HttpResponseClientRefresh
 
 from argus.auth.utils import get_preference, save_preference
@@ -16,26 +15,10 @@ LOG = logging.getLogger(__name__)
 THEMES_MODULE = "argus_htmx"
 
 
-class ThemeListView(ListView):
-    http_method_names = ["get", "post", "head", "options", "trace"]
-    template_name = "htmx/themes/themes_list.html"
-
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-        self.themes = THEME_NAMES
-
-    def get_queryset(self):
-        return self.themes
-
-    def post(self, request, *args, **kwargs):
-        save_preference(request, request.POST, "argus_htmx", "theme")
-        return HttpResponseRedirect("")
-
-
 @require_GET
 def theme_names(request: HtmxHttpRequest) -> HttpResponse:
     themes = THEME_NAMES
-    return render(request, "htmx/themes/theme_list.html", {"theme_list": themes})
+    return render(request, "htmx/themes/_theme_list.html", {"theme_list": themes})
 
 
 @require_POST
