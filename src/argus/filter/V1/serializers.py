a683935fc4b9af22d5a286d872f37982993baec5
Johanna England <johanna.england@sikt.no>
2024-11-28 14:11:42 +0100

    fixup! Remove unused third party imports

--- src/argus/filter/V1/serializers.py
+++ src/argus/filter/V1/serializers.py
@@ -1,7 +1,6 @@
 import json
 
 from rest_framework import serializers
-from rest_framework import serializers
 
 from argus.incident.constants import Level
 from argus.notificationprofile.models import Filter
