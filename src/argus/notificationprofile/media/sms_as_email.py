40bfbee51ab1aafa03b9d44c98cdf769f97ec1c7
Hanne Moa <hanne.moa@sikt.no>
2024-12-03 09:43:45 +0100

    fix final test problem

--- src/argus/notificationprofile/media/sms_as_email.py
+++ src/argus/notificationprofile/media/sms_as_email.py
@@ -54,6 +54,7 @@ class SMSNotification(NotificationMedium):
     class Form(forms.Form):
         phone_number = PhoneNumberField()
 
+    @classmethod
     def clean(cls, form: Form) -> Form:
         form.cleaned_data[cls.MEDIA_SETTINGS_KEY] = form.cleaned_data[cls.MEDIA_SETTINGS_KEY].as_e164
         return form
