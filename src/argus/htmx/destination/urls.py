stveit/argus-htmx-frontend

Add view for updating a destination

src/argus_htmx/destinations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import destinations, destinations_delete
+from .views import destinations, destinations_delete, destinations_update
 
 app_name = "htmx"
 urlpatterns = [
     path("", destinations, name="destinations"),
     path("<int:pk>/delete", destinations_delete, name="destinations-delete"),
+    path("<int:pk>/", destinations_update, name="destinations-update"),
 ]

Uninett/Argus

src/argus/htmx/destination/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import destination_list, destination_delete
+from .views import destination_list, destination_delete, destination_update
 
 app_name = "htmx"
 urlpatterns = [
     path("", destination_list, name="destination-list"),
     path("<int:pk>/delete/", destination_delete, name="destination-delete"),
+    path("<int:pk>/", destination_update, name="destination-update"),
 ]