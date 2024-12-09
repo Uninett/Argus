stveit/argus-htmx-frontend

Add view for destinations page

src/argus_htmx/destinations/views.py
@@ -1,0 +1,9 @@
+from django.shortcuts import render
+
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
+
+
+@require_http_methods(["GET"])
+def destinations(request) -> HttpResponse:
+    return render(request, "htmx/destinations/destinations.html")

Uninett/Argus

src/argus/htmx/destination/views.py
@@ -1,0 +1,13 @@
+from django.shortcuts import render
+
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
+
+
+@require_http_methods(["GET"])
+def destination_list(request) -> HttpResponse:
+    return _render_destination_list(request)
+
+
+def _render_destination_list(request) -> HttpResponse:
+    return render(request, "htmx/destination/destination_list.html")