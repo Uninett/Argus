fe961ed6524c12d5649104a3f45c2da26d190340
Simon Oliver Tveit <simtve@uninett.no>
2025-01-10 16:07:27 +0100

    Remove unused parameter
    After changes to update_htmx, this part is no longer used

--- src/argus/htmx/destination/views.py
+++ src/argus/htmx/destination/views.py
@@ -57,21 +57,16 @@ def update_htmx(request, pk: int) -> HttpResponse:
 def _render_destination_list(
     request,
     create_form: Optional[DestinationFormCreate] = None,
-    update_forms: Optional[Sequence[DestinationFormUpdate]] = None,
     template: str = "htmx/destination/destination_list.html",
 ) -> HttpResponse:
     """Function to render the destinations page.
 
     :param create_form: this is used to display the form for creating a new destination
-    with errors while retaining the user input. If you want a blank form, pass None.
-    :param update_forms: list of update forms to display. Useful for rendering forms
-    with error messages while retaining the user input.
-    If this is None, the update forms will be generated from the user's destinations."""
+    with errors while retaining the user input. If you want a blank form, pass None."""
 
     if create_form is None:
         create_form = DestinationFormCreate()
-    if update_forms is None:
-        update_forms = _get_update_forms(request.user)
+    update_forms = _get_update_forms(request.user)
     grouped_forms = _group_update_forms_by_media(update_forms)
     context = {
         "create_form": create_form,
