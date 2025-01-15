98b1327b7d6063bf9f5cc32c6c2326f20a50c9a7
Hanne Moa <hanne.moa@sikt.no>
2025-01-15 14:53:23 +0100

    Make the profiles delete button a modal

---
+++ src/argus/htmx/modals.py
@@ -0,0 +1,34 @@
+from typing import ClassVar, Any
+
+from django.forms.renderers import get_default_renderer
+from django.forms.utils import RenderableMixin
+
+# pydantic instead of dataclasses because the former has
+# less surprises when subclassing
+from pydantic import BaseModel
+
+
+class Modal(RenderableMixin, BaseModel):
+    renderer: ClassVar[Any] = get_default_renderer()
+
+    model_config = {"arbitrary_types_allowed": True}
+
+    template_name: str = "htmx/_base_form_modal_experimental.html"
+
+    # needed by template
+    button_class: str = "btn-primary"
+    cancel_text: str = "Cancel"
+    submit_text: str = "Ok"
+    button_title: str
+    dialog_id: str
+    endpoint: str
+    explanation: str
+    header: str
+
+    def get_context(self):
+        modal_dict = dict(self)
+        return {"modal": modal_dict}
+
+
+class ConfirmationModal(Modal):
+    pass
