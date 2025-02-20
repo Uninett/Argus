e24afcb9daea89178f30430e9deb7ac55e053c7e
Hanne Moa <hanne.moa@sikt.no>
2025-02-20 14:41:54 +0100

    Use <time>-tag for incident datetimes

--- src/argus/htmx/incident/views.py
+++ src/argus/htmx/incident/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+from django.utils.timezone import now as tznow
 from django.shortcuts import render, get_object_or_404
 
 from django.views.decorators.http import require_POST, require_GET
@@ -59,6 +60,14 @@ def prefetch_incident_daughters():
 @require_GET
 def incident_detail(request, pk: int):
     incident = get_object_or_404(Incident, id=pk)
+    duration = None
+    now = tznow()
+    if incident.end_time:
+        if incident.end_time <= now:
+            duration = incident.end_time - incident.start_time
+        else:
+            duration = now - incident.start_time
+    incident.duration = duration
     context = {
         "incident": incident,
         "page_title": str(incident),
