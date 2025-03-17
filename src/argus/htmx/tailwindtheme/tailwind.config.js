a9198caa6710fb11347db5c5fd307826fe0f0ec8
Simon Oliver Tveit <simtve@uninett.no>
2025-03-17 10:49:06 +0100

    Add unique colors per severity level

--- src/argus/htmx/tailwindtheme/tailwind.config.js
+++ src/argus/htmx/tailwindtheme/tailwind.config.js
@@ -22,6 +22,21 @@ module.exports = {
             '8': '8px',
         },
         ...{},
+        extend:{
+            colors: {
+                "severity-primary-1": "red",
+                "severity-primary-2": "orange",
+                "severity-primary-3": "yellow",
+                "severity-primary-4": "green",
+                "severity-primary-5": "blue",
+                "severity-secondary-1": "white",
+                "severity-secondary-2": "black",
+                "severity-secondary-3": "black",
+                "severity-secondary-4": "white",
+                "severity-secondary-5": "white",
+
+            },
+        },
     },
     safelist: [
         "htmx-request"
