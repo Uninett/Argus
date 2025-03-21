76239266c389925781c41bebd678042fe0226105
Hanne Moa <hanne.moa@sikt.no>
2025-03-21 09:16:14 +0100

    Get rid of console warning when deleting timeslot

--- src/argus/htmx/timeslot/views.py
+++ src/argus/htmx/timeslot/views.py
@@ -256,6 +256,9 @@ class TimeslotUpdateView(FormsetMixin, TimeslotMixin, UpdateView):
 
 
 class TimeslotDeleteView(TimeslotMixin, DeleteView):
+    def post(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         success_url = self.get_success_url()
