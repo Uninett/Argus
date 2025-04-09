14e1dee38f3d7d5d1876f806e66ccbdbbc39e6d0
Hanne Moa <hanne.moa@sikt.no>
2025-04-09 11:14:30 +0200

    add a "timeframe" preference

--- src/argus/htmx/incident/constants.py
+++ src/argus/htmx/incident/constants.py
@@ -8,7 +8,13 @@ __all__ = [
     "UPDATE_INTERVAL_DEFAULT",
     "UPDATE_INTERVAL_ALLOWED",
     "UPDATE_INTERVAL_CHOICES",
+    "TIMEFRAME_DEFAULT",
+    "TIMEFRAME_CHOICES",
 ]
+
+_no_timeframe = 0
+_hour = 60
+_day = _hour * 24
 
 
 DEFAULT_PAGE_SIZE = getattr(settings, "ARGUS_INCIDENTS_DEFAULT_PAGE_SIZE", 10)
@@ -18,3 +24,14 @@ PAGE_SIZE_CHOICES = tuple((ps, ps) for ps in ALLOWED_PAGE_SIZES)
 UPDATE_INTERVAL_DEFAULT = getattr(settings, "ARGUS_INCIDENTS_UPDATE_INTERVAL_DEFAULT", 30)
 UPDATE_INTERVAL_ALLOWED = getattr(settings, "ARGUS_INCIDENTS_UPDATE_INTERVALS", ["never", 5, 30, 60])
 UPDATE_INTERVAL_CHOICES = tuple((interval, interval) for interval in UPDATE_INTERVAL_ALLOWED)
+
+TIMEFRAME_DEFAULT = _no_timeframe
+TIMEFRAME_CHOICES = (
+    (_no_timeframe, "No timeframe"),
+    (_hour, "Last hour"),
+    (_hour * 3, "Last 3 hours"),
+    (_hour * 12, "Last 12 hours"),
+    (_day, "Last 24 hours"),
+    (_day * 7, "Last week"),
+    (_day * 30, "Last 30 days"),
+)
