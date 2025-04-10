d3935cb1741ce65026100f6496bb9f74626c0b49
Hanne Moa <hanne.moa@sikt.no>
2025-04-10 13:29:12 +0200

    Pretty print update_interval everywhere

--- src/argus/htmx/user/preferences/models.py
+++ src/argus/htmx/user/preferences/models.py
@@ -14,10 +14,10 @@ from argus.htmx.constants import (
     THEME_CHOICES,
     THEME_DEFAULT,
     THEME_NAMES,
-    UPDATE_INTERVAL_ALLOWED,
     UPDATE_INTERVAL_DEFAULT,
     UPDATE_INTERVAL_CHOICES,
 )
+from argus.htmx.incident.utils import update_interval_string
 
 
 class DateTimeFormatForm(forms.Form):
@@ -56,7 +56,7 @@ class ArgusHtmxPreferences:
             partial_response_template="htmx/user/_current_theme.html",
         ),
         "update_interval": PreferenceField(
-            form=UpdateIntervalForm, default=UPDATE_INTERVAL_DEFAULT, choices=UPDATE_INTERVAL_ALLOWED
+            form=UpdateIntervalForm, default=UPDATE_INTERVAL_DEFAULT, choices=UPDATE_INTERVAL_CHOICES
         ),
         "incidents_table_layout": PreferenceField(
             form=IncidentsTableLayout, default=INCIDENTS_TABLE_LAYOUT_DEFAULT, choices=INCIDENTS_TABLE_LAYOUT_ALLOWED
@@ -67,9 +67,11 @@ class ArgusHtmxPreferences:
         datetime_format_name = context.get("datetime_format_name", DATETIME_DEFAULT)
         datetime_format = DATETIME_FORMATS[datetime_format_name]
         incidents_table_layout = context.get("incidents_table_layout", INCIDENTS_TABLE_LAYOUT_DEFAULT)
+        update_interval = context.get("update_interval", UPDATE_INTERVAL_DEFAULT)
         return {
             "datetime_format": datetime_format.datetime,
             "date_format": datetime_format.date,
             "time_format": datetime_format.time,
             "incidents_table_layout_compact": incidents_table_layout == "compact",
+            "update_interval_pp": update_interval_string(update_interval),
         }
