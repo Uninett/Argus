85a48c021aaf0a008722a1e4a55ada9feb3c0261
PelleK <elfjes@users.noreply.github.com>
2025-04-22 09:22:22 +0200

    Use simple email defaults in base, tls in prod

--- src/argus/site/settings/prod.py
+++ src/argus/site/settings/prod.py
@@ -2,3 +2,5 @@ from argus.spa.settings import *
 
 # Beware: setting this to something else in production has security implications
 INTERNAL_IPS = []  # Don't alter me, please
+EMAIL_PORT = get_int_env("EMAIL_PORT", 587)
+EMAIL_USE_TLS = get_bool_env("EMAIL_USE_TLS", default=True)
