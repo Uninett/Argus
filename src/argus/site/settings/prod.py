b849c2f908b5f1902f55dc4817a7ecaf76621abe
Hanne Moa <hanne.moa@sikt.no>
2025-04-23 19:42:58 +0200

    Make the prod settings support the new frontend

--- src/argus/site/settings/prod.py
+++ src/argus/site/settings/prod.py
@@ -1,4 +1,10 @@
-from argus.spa.settings import *
+from argus.site.utils import update_settings
+from argus.site.settings.backend import *
+from argus.htmx.appconfig import APP_SETTINGS
+
+update_settings(globals(), APP_SETTINGS)
+
+ROOT_URLCONF = "argus.htmx.root_urls"
 
 # Beware: setting this to something else in production has security implications
 INTERNAL_IPS = []  # Don't alter me, please
