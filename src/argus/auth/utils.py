83d1541cda4607a04c4920e6bdf6f262399919f2
Hanne Moa <hanne.moa@sikt.no>
2025-05-12 08:02:57 +0200

    stop1

--- src/argus/auth/utils.py
+++ src/argus/auth/utils.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from functools import cache
 import logging
 from typing import Mapping, Union
 
@@ -8,19 +9,28 @@ from django.contrib import messages
 from django.http import HttpRequest
 from django.utils.module_loading import import_string
 
-from social_core.backends.oauth import BaseOAuth2
-
 from argus.auth.models import Preferences, SessionPreferences
+
+# importing is used to check for existence
+# ruff: noqa: F401
 
 
 __all__ = [
+    "get_auth_backend_readiness",
     "get_authentication_backend_classes",
-    "has_model_backend",
-    "has_remote_user_backend",
-    "get_psa_authentication_backends",
-    "get_preference_obj",
-    "get_preference",
+    "get_model_backend",
     "get_or_update_preference",
+    "get_preference",
+    "get_preference_obj",
+    "get_psa_authentication_backends",
+    "get_remote_user_backend",
+    "has_allauth_socialaccount_backend",
+    "has_psa_backend",
+    "is_allauth_installed_correctly",
+    "is_allauth_mfa_installed_correctly",
+    "is_allauth_socialaccount_installed_correctly",
+    "is_psa_installed_correctly",
+    "pop_auth_backend",
     "save_preferences",
 ]
 
@@ -28,23 +38,126 @@ __all__ = [
 LOG = logging.getLogger(__name__)
 
 
+@cache
 def get_authentication_backend_classes():
-    backend_dotted_paths = getattr(settings, "AUTHENTICATION_BACKENDS")
+    """Convert setting of strings to list of classes and cache
+
+    Make a copy of the result if altering the list!
+    """
+    backend_dotted_paths = getattr(settings, "AUTHENTICATION_BACKENDS", [])
     backends = [import_string(path) for path in backend_dotted_paths]
     return backends
 
 
-def has_model_backend(backends):
-    return ModelBackend in backends
+def pop_auth_backend(backends, backend):
+    try:
+        location = backends.index(backend)
+    except ValueError:
+        return None
+    try:
+        backend = backends.pop(location)
+    except IndexError:
+        return None
+    return backend
 
 
-def has_remote_user_backend(backends):
-    return RemoteUserBackend in backends
+def get_model_backend(backends):
+    return pop_auth_backend(backends, ModelBackend)
+
+
+def get_remote_user_backend(backends):
+    return pop_auth_backend(backends, RemoteUserBackend)
+
+
+# detect and set psa, allauth
+
+
+@cache
+def is_psa_installed_correctly():
+    try:
+        from social_core.backends.oauth import BaseOAuth2
+    except ImportError:
+        return False
+    return "social_django" in settings.INSTALLED_APPS
+
+
+@cache
+def is_allauth_installed_correctly():
+    try:
+        import allauth
+        from allauth import account
+    except ImportError:
+        return False
+    if "allauth" not in settings.INSTALLED_APPS:
+        return False
+    return "allauth.account" in settings.INSTALLED_APPS
+
+
+def is_allauth_mfa_installed_correctly():
+    if not is_allauth_installed_correctly():
+        return False
+    try:
+        from allauth import mfa
+    except ImportError:
+        return False
+    return "allauth.mfa" in settings.INSTALLED_APPS
+
+
+def is_allauth_socialaccount_installed_correctly():
+    MAGIC_APP = "allauth.socialaccount.providers"
+    if not is_allauth_installed_correctly():
+        return False
+    if "allauth.socialaccount" not in settings.INSTALLED_APPS:
+        return False
+    for app in settings.INSTALLED_APPS:
+        if app.startswith(MAGIC_APP) and len(app) > len(MAGIC_APP):
+            return True
+    return False
+
+
+def get_allauth_backend(backends):
+    if not is_allauth_installed_correctly():
+        return None
+    try:
+        from allauth.account.auth_backends import AuthenticationBackend
+    except ImportError:
+        return None
+    return pop_auth_backend(backends, AuthenticationBackend)
+
+
+def has_allauth_socialaccount_backend():
+    if not is_allauth_socialaccount_installed_correctly():
+        return False
+    SOCIALACCOUNT_PROVIDERS = getattr(settings, "SOCIALACCOUNT_PROVIDERS", {})
+    return bool(SOCIALACCOUNT_PROVIDERS)
 
 
 def get_psa_authentication_backends(backends=None):
+    if not is_psa_installed_correctly():
+        return []
     backends = backends if backends else get_authentication_backend_classes()
+    from social_core.backends.oauth import BaseOAuth2
+
     return [backend for backend in backends if issubclass(backend, BaseOAuth2)]
+
+
+def has_psa_backend():
+    return bool(get_psa_authentication_backends())
+
+
+def get_auth_backend_readiness():
+    backends = getattr(settings, "AUTHENTICATION_BACKENDS", [])
+    backend_map = {
+        "django-user": bool(get_model_backend(backends)),
+        "remote-user": bool(get_remote_user_backend(backends)),
+        "allauth": bool(get_allauth_backend(backends)),
+        "allauth-social": has_allauth_socialaccount_backend(),
+        "psa": has_psa_backend(),
+    }
+    return backend_map
+
+
+# preferences
 
 
 def get_preference_obj(request, namespace) -> Preferences:
