6722a68153ac65bfb011c1a2b8c82e3139475ff7
Johanna England <johanna.england@sikt.no>
2025-05-19 13:11:02 +0200

    Add more tests for create source CLI command

--- tests/dev/test_create_source.py
+++ tests/dev/test_create_source.py
@@ -1,7 +1,8 @@
 from django.core.management import call_command
 from django.test import TestCase
 
-from argus.incident.models import SourceSystem
+from argus.auth.models import User
+from argus.incident.models import SourceSystem, SourceSystemType
 from argus.util.testing import connect_signals, disconnect_signals
 
 
@@ -26,3 +27,40 @@ class CreateSourceTests(TestCase):
         call_command("create_source", source_name, source_type=source_type)
 
         self.assertTrue(SourceSystem.objects.exclude(id__in=previous_source_pks).filter(type_id=source_type).exists())
+
+    def test_create_source_will_use_already_existing_user(self):
+        previous_source_pks = [source.id for source in SourceSystem.objects.all()]
+        source_name = "source name"
+        existing_user = User.objects.create(username=source_name, is_staff=False, is_superuser=False)
+
+        call_command("create_source", source_name)
+
+        source = SourceSystem.objects.exclude(id__in=previous_source_pks).filter(name=source_name).first()
+        self.assertTrue(source)
+        self.assertEqual(source.user_id, existing_user.id)
+
+    def test_create_source_will_use_already_existing_source_system_type(self):
+        previous_source_pks = [source.id for source in SourceSystem.objects.all()]
+        source_name = "source name"
+        source_type_name = "source type name"
+        existing_source_type = SourceSystemType.objects.create(name=source_type_name)
+
+        call_command("create_source", source_name, source_type=source_type_name)
+
+        source = SourceSystem.objects.exclude(id__in=previous_source_pks).filter(name=source_name).first()
+        self.assertTrue(source)
+        self.assertEqual(source.type_id, existing_source_type.name)
+
+    def test_create_source_will_use_already_existing_source_system(self):
+        previous_source_pks = [source.id for source in SourceSystem.objects.all()]
+        source_name = "source name"
+        source_type_name = "source type name"
+        source_user = User.objects.create(username=source_name, is_staff=False, is_superuser=False)
+        source_type = SourceSystemType.objects.create(name=source_type_name)
+        source = SourceSystem.objects.create(name=source_name, type=source_type, user=source_user)
+
+        call_command("create_source", source_name)
+
+        cli_source = SourceSystem.objects.exclude(id__in=previous_source_pks).filter(name=source_name).first()
+        self.assertTrue(cli_source)
+        self.assertEqual(cli_source.id, source.id)
