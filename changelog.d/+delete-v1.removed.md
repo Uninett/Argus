eb5e596b05d683568ee4238a88fca90f70985646
Hanne Moa <hanne.moa@sikt.no>
2025-05-23 14:03:28 +0200

    Actually delete API version v1 and its tests and docs for old frontend (#1446)
    * Delete tests/V1
    * Delete src/arcus/spa/
    * Delete API v1
    * Delete docs/reference/react-frontend.rst
    * Delete docs/reference/api/v1.rst

---
+++ changelog.d/+delete-v1.removed.md
@@ -0,0 +1,2 @@
+Deleted API v1, its tests and mentions in the documentation. Ditto all support
+for the old frontend.
--- src/argus/spa/root_urls.py
+++
@@ -1,6 +0,0 @@
-from argus.site.urls import urlpatterns
-
-from .spa_urls import urlpatterns as spa_urlpatterns
-
-
-urlpatterns += spa_urlpatterns
