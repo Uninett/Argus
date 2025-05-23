66eaf4b50d31106c958b5d221b0a73be8a423c3b
podliashanyk <60876078+podliashanyk@users.noreply.github.com>
2025-05-23 14:32:36 +0200

    Add settable banner to all pages
    Useful for special versions of argus: demo, review sites in CI/CD...
    
    * Add new Django setting for displaying banner message
    * Add context processor for banner message
    * Add banner to the base HTML template

--- src/argus/htmx/appconfig.py
+++ src/argus/htmx/appconfig.py
@@ -36,6 +36,7 @@ _app_settings = [
         "context_processors": [
             "argus.auth.context_processors.preferences",
             "argus.htmx.context_processors.static_paths",
+            "argus.htmx.context_processors.banner_message",
         ],
         "middleware": {
             "argus.htmx.middleware.LoginRequiredMiddleware": "end",
