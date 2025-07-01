7d44a258e44eb1025029075babedd782c31c4e68
Hanne Moa <hanne.moa@sikt.no>
2025-07-01 11:00:00 +0200

    Pretty print update_interval everywhere
    Always show the human readable interval name instead of just a number.

--- src/argus/htmx/incident/constants.py
+++ src/argus/htmx/incident/constants.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from argus.htmx.incident.utils import update_interval_string
+
 
 __all__ = [
     "DEFAULT_PAGE_SIZE",
@@ -9,7 +11,6 @@ __all__ = [
     "INCIDENTS_TABLE_LAYOUT_CHOICES",
     "INCIDENTS_TABLE_LAYOUT_DEFAULT",
     "UPDATE_INTERVAL_DEFAULT",
-    "UPDATE_INTERVAL_ALLOWED",
     "UPDATE_INTERVAL_CHOICES",
     "TIMEFRAME_DEFAULT",
     "TIMEFRAME_CHOICES",
@@ -24,9 +25,10 @@ DEFAULT_PAGE_SIZE = getattr(settings, "ARGUS_INCIDENTS_DEFAULT_PAGE_SIZE", 10)
 ALLOWED_PAGE_SIZES = getattr(settings, "ARGUS_INCIDENTS_PAGE_SIZES", [10, 20, 50, 100])
 PAGE_SIZE_CHOICES = tuple((ps, ps) for ps in ALLOWED_PAGE_SIZES)
 
+_UPDATE_INTERVAL_FALLBACK = ["never", 5, 30, 60]
 UPDATE_INTERVAL_DEFAULT = getattr(settings, "ARGUS_INCIDENTS_UPDATE_INTERVAL_DEFAULT", 30)
-UPDATE_INTERVAL_ALLOWED = getattr(settings, "ARGUS_INCIDENTS_UPDATE_INTERVALS", ["never", 5, 30, 60])
-UPDATE_INTERVAL_CHOICES = tuple((interval, interval) for interval in UPDATE_INTERVAL_ALLOWED)
+_UPDATE_INTERVALS = getattr(settings, "ARGUS_INCIDENTS_UPDATE_INTERVALS", _UPDATE_INTERVAL_FALLBACK)
+UPDATE_INTERVAL_CHOICES = tuple((interval, update_interval_string(interval)) for interval in _UPDATE_INTERVALS)
 
 TIMEFRAME_DEFAULT = _no_timeframe
 TIMEFRAME_CHOICES = (
