a2761222ce6408d8427fde17b1464d93a4d88d7d
Hanne Moa <hanne.moa@sikt.no>
2025-08-18 09:14:31 +0200

    move argus theme to separate css file

--- src/argus/htmx/tailwindtheme/tailwind.config.js
+++ src/argus/htmx/tailwindtheme/tailwind.config.js
@@ -1,8 +1,7 @@
 const projectPaths = [
     '../templates/**/*.html',
     './**/templates/**/*.html',
-    'src/argus_htmx/templates/**/*.html',
-    '!**/templates/**/admin/**'
+    'src/argus_htmx/templates/**/*.html'
 ];
 
 const contentPaths = [...projectPaths];
@@ -22,21 +21,6 @@ module.exports = {
             '8': '8px',
         },
         ...{},
-        extend:{
-            colors: {
-                "severity-primary-1": "red",
-                "severity-primary-2": "orange",
-                "severity-primary-3": "yellow",
-                "severity-primary-4": "green",
-                "severity-primary-5": "blue",
-                "severity-secondary-1": "white",
-                "severity-secondary-2": "black",
-                "severity-secondary-3": "black",
-                "severity-secondary-4": "white",
-                "severity-secondary-5": "white",
-
-            },
-        },
     },
     safelist: [
         "htmx-request"
