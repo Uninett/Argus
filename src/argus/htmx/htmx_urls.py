306b555b00b22ffa6042f24ef0d1c6a71528089b
Hanne Moa <hanne.moa@sikt.no>
2025-10-07 11:31:15 +0200

    Use allauth for all logins

--- src/argus/htmx/htmx_urls.py
+++ src/argus/htmx/htmx_urls.py
@@ -1,11 +1,15 @@
 from argus.htmx.appconfig import APP_SETTINGS
 from argus.site.utils import get_urlpatterns
-from argus.util.app_utils import is_using_psa
+from argus.util.app_utils import is_using_psa, is_using_allauth
 
+
+account_urlpatterns = []
 urlpatterns = get_urlpatterns(APP_SETTINGS)
 
 if is_using_psa():
     from argus.auth.psa.urls import urlpatterns as account_urlpatterns
+elif is_using_allauth():
+    from argus.auth.allauth.urls import urlpatterns as account_urlpatterns
 else:
     from argus.htmx.auth.urls import urlpatterns as account_urlpatterns
 
--- src/argus/util/app_utils.py
+++ src/argus/util/app_utils.py
@@ -6,3 +6,9 @@ def is_using_psa():
     turn_on_psa = getattr(settings, "USE_PYTHON_SOCIAL_AUTH", True)
     psa_is_installed = apps.is_installed("social_django")
     return turn_on_psa and psa_is_installed
+
+
+def is_using_allauth():
+    turn_on_psa = getattr(settings, "USE_PYTHON_SOCIAL_AUTH", True)
+    allauth_is_installed = apps.is_installed("allauth")
+    return allauth_is_installed and not turn_on_psa
