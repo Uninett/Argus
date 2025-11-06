3cd2df5539e9a39db74a82dfbedd70376ca081d4
Hanne Moa <hanne.moa@sikt.no>
2025-11-06 15:29:52 +0100

    Use forms for all incident list GET parameters
    * Add debug logs for incident filtering
    * Dedupe GET-parameters to incident_list view
    * Keep all existing GET-parameters on page change and auto refresh
    * Fix disappearing timeframe GET-parameter
    * Use a template for the timeframe widget
    * Use querystring template tag
    * Use forms for all GET-parameters (treat non-filterbox GET params more consistently)
      * Use a form for page_size
      * Split up incident list forms into types
      * Get initial form input from session, user preferences
      * Store form result in session, user preferences

---
+++ src/argus/htmx/incident/forms/incident_filters.py
@@ -0,0 +1,86 @@
+from datetime import timedelta
+
+from django import forms
+from django.utils.timezone import now as tznow
+
+from argus.htmx.constants import (
+    PAGE_SIZE_CHOICES,
+    PAGE_SIZE_DEFAULT,
+    TIMEFRAME_CHOICES,
+    TIMEFRAME_DEFAULT,
+)
+from argus.htmx.incident.forms.base import IncidentListForm
+
+
+# incident list filter widgets
+
+
+class IncidentListRefreshInfoSelect(forms.Select):
+    template_name = "htmx/components/incident_list_refresh_info_select.html"
+
+
+# incident filter forms
+
+
+class PageNumberForm(IncidentListForm):
+    fieldname = "page"
+    field_initial = 1
+
+    page = forms.IntegerField(required=False, initial=field_initial, min_value=field_initial)
+
+
+# Stored in preference
+class PageSizeForm(IncidentListForm):
+    fieldname = "page_size"
+    field_initial = PAGE_SIZE_DEFAULT
+
+    page_size = forms.TypedChoiceField(
+        required=False,
+        choices=PAGE_SIZE_CHOICES,
+        coerce=int,
+        initial=field_initial,
+        widget=IncidentListRefreshInfoSelect,
+    )
+
+    # page_size is used by a paginator, no need to define ``filter``
+
+    def get_clean_value(self, request):
+        # breakpoint()
+        if request.GET.get(self.fieldname, 0):
+            return super().get_clean_value(request)
+        return self.get_initial_value(request)
+
+    @classmethod
+    def get_initial_value(cls, request):
+        return cls._get_initial_preference_value(request, "argus_htmx")
+
+    def store(self, request):
+        return self._store_preference(request, "argus_htmx")
+
+
+# Stored in session to ensure it does not go missing
+class TimeframeForm(IncidentListForm):
+    fieldname = "timeframe"
+    field_initial = TIMEFRAME_DEFAULT
+
+    timeframe = forms.TypedChoiceField(
+        required=False,
+        choices=TIMEFRAME_CHOICES,
+        coerce=int,
+        initial=field_initial,
+        widget=IncidentListRefreshInfoSelect,
+    )
+
+    def filter(self, queryset, request):
+        timeframe = self.get_clean_value(request)
+        if timeframe:
+            after = tznow() - timedelta(minutes=timeframe)
+            queryset = queryset.filter(start_time__gte=after)
+        return queryset
+
+    @classmethod
+    def get_initial_value(cls, request):
+        return request.session.get("timeframe", cls.field_initial) or cls.field_initial
+
+    def store(self, request):
+        return self._store_in_session(request)
