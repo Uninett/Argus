b3b2ffd4765d2787233790d8400f8b5e95fda216
Hanne Moa <hanne.moa@sikt.no>
2025-11-12 15:39:51 +0100

    Update and add tests

---
+++ src/argus/htmx/user/factories.py
@@ -0,0 +1,13 @@
+import factory
+
+from argus.auth.factories import PersonUserFactory
+from argus.htmx.user.preferences.models import ArgusHtmxPreferences
+
+
+class ArgusHtmxPreferencesFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ArgusHtmxPreferences
+
+    user = PersonUserFactory()
+    namespace = "argus_htmx"
+    preferences = dict()
