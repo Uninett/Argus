76c881f3697332a92e94e67c87714717b9aead32
Johanna England <johanna.england@sikt.no>
2025-11-20 13:57:43 +0100

    Add planned maintenance functionality to admin
    Add extra logging for changes to PMs

---
+++ src/argus/plannedmaintenance/admin.py
@@ -0,0 +1,42 @@
+import logging
+
+from django.contrib import admin
+
+from .models import PlannedMaintenanceTask
+
+LOG = logging.getLogger(__name__)
+
+
+class PlannedMaintenanceTaskAdmin(admin.ModelAdmin):
+    # Planned maintenance list
+    list_display = (
+        "owner",
+        "created",
+        "start_time",
+        "end_time",
+        "description",
+    )
+    search_fields = (
+        "description",
+        "owner__first_name",
+        "owner__last_name",
+        "owner__username",
+    )
+
+    list_select_related = ("owner",)
+    raw_id_fields = ("owner",)
+    ordering = ("start_time",)
+
+    # Planned maintenance adding/editing
+    filter_horizontal = ("filters",)
+    readonly_fields = ["created"]
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if change:
+            LOG.debug("Planned maintenance %s changed by %s", obj.id, request.user)
+        else:
+            LOG.debug("Planned maintenance %s created by %s", obj.id, request.user)
+
+
+admin.site.register(PlannedMaintenanceTask, PlannedMaintenanceTaskAdmin)
