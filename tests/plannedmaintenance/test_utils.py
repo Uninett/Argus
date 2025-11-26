b4ee2fa0ef355f92a86b9b46f5806edaf1aca0ec
Johanna England <johanna.england@sikt.no>
2025-11-26 14:26:27 +0100

    Add tests for planned maintenance

--- tests/plannedmaintenance/test_utils.py
+++ tests/plannedmaintenance/test_utils.py
@@ -4,10 +4,14 @@ from django.test import TestCase, tag
 from django.utils import timezone
 
 from argus.filter.factories import FilterFactory
-from argus.incident.factories import StatefulIncidentFactory
-from argus.incident.models import Incident
+from argus.incident.factories import EventFactory, StatefulIncidentFactory
+from argus.incident.models import Event, Incident
 from argus.plannedmaintenance.factories import PlannedMaintenanceFactory
-from argus.plannedmaintenance.utils import incidents_covered_by_planned_maintenance_task
+from argus.plannedmaintenance.models import PlannedMaintenanceTask
+from argus.plannedmaintenance.utils import (
+    event_covered_by_planned_maintenance,
+    incidents_covered_by_planned_maintenance_task,
+)
 from argus.util.testing import disconnect_signals, connect_signals
 
 
@@ -55,3 +59,55 @@ class TestIncidentsCoveredByPlannedMaintenanceTask(TestCase):
 
         self.assertIn(important_incident, incidents)
         self.assertNotIn(self.open_incident, incidents)
+
+
+@tag("database")
+class TestEventCoveredByPlannedMaintenanceTasks(TestCase):
+    def setUp(self):
+        disconnect_signals()
+        self.start_event = EventFactory(type=Event.Type.INCIDENT_START)
+        self.current_pm = PlannedMaintenanceFactory()
+        self.maxlevel_filter = FilterFactory(filter={"maxlevel": 5})
+
+    def teardown(self):
+        connect_signals()
+
+    def test_given_relevant_event_return_true(self):
+        self.current_pm.filters.add(self.maxlevel_filter)
+
+        covered = event_covered_by_planned_maintenance(self.start_event)
+
+        self.assertTrue(covered)
+
+    def test_given_wrong_event_type_return_false(self):
+        self.current_pm.filters.add(FilterFactory(filter={"event_type": Event.Type.ACKNOWLEDGE}))
+
+        covered = event_covered_by_planned_maintenance(self.start_event)
+
+        self.assertFalse(covered)
+
+    def test_given_wrong_maxlevel_return_false(self):
+        self.current_pm.filters.add(FilterFactory(filter={"maxlevel": 1}))
+
+        covered = event_covered_by_planned_maintenance(self.start_event)
+
+        self.assertFalse(covered)
+
+    def test_given_pm_with_multiple_filters_only_matching_one_filter_return_false(self):
+        self.current_pm.filters.add(FilterFactory(filter={"maxlevel": 1}))
+        self.current_pm.filters.add(FilterFactory(filter={"event_type": Event.Type.INCIDENT_START}))
+
+        covered = event_covered_by_planned_maintenance(self.start_event)
+
+        self.assertFalse(covered)
+
+    def test_given_no_pms_active_at_time_return_false(self):
+        now = timezone.now()
+        past_pm = PlannedMaintenanceFactory(start_time=now - timedelta(days=5), end_time=now - timedelta(days=4))
+        past_pm.filters.add(self.maxlevel_filter)
+
+        covered = event_covered_by_planned_maintenance(
+            self.start_event, PlannedMaintenanceTask.objects.filter(id=past_pm.id)
+        )
+
+        self.assertFalse(covered)
