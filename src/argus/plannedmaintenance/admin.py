0178f114cf6167c2fb47a52cd1c4dc308cefcf73
Johanna England <johanna.england@sikt.no>
2025-11-26 13:16:08 +0100

    Add functionality to cancel PMs to admin
    Cancel selected ones in the admin list and single ones in the edit view

--- src/argus/plannedmaintenance/admin.py
+++ src/argus/plannedmaintenance/admin.py
@@ -2,6 +2,8 @@ import logging
 
 from django.contrib import admin
 from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.urls import path, reverse
 from django.utils import timezone
 
 from argus.util.admin_utils import list_filter_factory
@@ -9,6 +11,14 @@ from argus.util.admin_utils import list_filter_factory
 from .models import PlannedMaintenanceTask
 
 LOG = logging.getLogger(__name__)
+
+
+@admin.action(description="Mark selected planned maintenance tasks as finished")
+def cancel_pms(modeladmin, request, queryset):
+    now = timezone.now()
+    queryset = queryset.filter(end_time__gt=now)
+    for pm in queryset:
+        pm.cancel(end_time=now)
 
 
 class PlannedMaintenanceTaskAdmin(admin.ModelAdmin):
@@ -52,9 +62,13 @@ class PlannedMaintenanceTaskAdmin(admin.ModelAdmin):
     raw_id_fields = ("owner",)
     ordering = ("start_time",)
 
+    actions = [cancel_pms]
+
     # Planned maintenance adding/editing
     filter_horizontal = ("filters",)
     readonly_fields = ["created"]
+
+    change_form_template = "plannedmaintenance/admin/planned_maintenance_change_form.html"
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -66,6 +80,23 @@ class PlannedMaintenanceTaskAdmin(admin.ModelAdmin):
             LOG.debug("Planned maintenance %s changed by %s", obj.id, request.user)
         else:
             LOG.debug("Planned maintenance %s created by %s", obj.id, request.user)
+
+    def get_urls(self):
+        orig_urls = super().get_urls()
+        urls = [
+            path(
+                "<int:pk>/cancel/",
+                self.admin_site.admin_view(self.cancel_planned_maintenance_task),
+                name="cancel-pm-task",
+            )
+        ]
+        return urls + orig_urls
+
+    def cancel_planned_maintenance_task(self, request, pk: int):
+        pm = PlannedMaintenanceTask.objects.get(id=pk)
+        pm.cancel()
+
+        return HttpResponseRedirect(reverse("admin:argus_plannedmaintenance_plannedmaintenancetask_changelist"))
 
 
 admin.site.register(PlannedMaintenanceTask, PlannedMaintenanceTaskAdmin)
