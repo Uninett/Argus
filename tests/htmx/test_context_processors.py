4f08c178027ed4f939cc1848a85eed70da2497b4
aleksfl <aleksander.flotten@sikt.no>
2025-12-02 10:27:48 +0100

    Update tests/htmx/test_context_processors.py
    Co-authored-by: Hanne Moa <hanne.moa@sikt.no>

--- tests/htmx/test_context_processors.py
+++ tests/htmx/test_context_processors.py
@@ -5,7 +5,7 @@ from django.test import RequestFactory
 from argus.htmx.context_processors import metadata
 
 
-class ContextProcessorsTest(unittest.TestCase):
+class MetadataContextProcessorTest(unittest.TestCase):
     def test_metadata_returns_str_version(self):
         request = RequestFactory().get("/foo")
         version = metadata(request)["version"]
