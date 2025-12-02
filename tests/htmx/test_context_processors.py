dbf365e2d7b424d4d55dadf33ac49aa28cb76303
Aleksander Fløtten <aleksander.flotten@sikt.no>
2025-12-02 09:56:52 +0100

    Add context processor test

---
+++ tests/htmx/test_context_processors.py
@@ -0,0 +1,13 @@
+import unittest
+
+from django.test import RequestFactory
+
+from argus.htmx.context_processors import metadata
+
+
+class ContextProcessorsTest(unittest.TestCase):
+    def test_metadata_returns_str_version(self):
+        request = RequestFactory().get("/foo")
+        version = metadata(request)["version"]
+        version_type = type(version)
+        self.assertTrue(isinstance(version, str), f'"{version}" is not str but {version_type}')
