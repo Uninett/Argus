a65b3534ae83ddc7302ea17ff8bbcdc7ac804da8
Hanne Moa <hanne.moa@sikt.no>
2025-12-04 10:15:53 +0100

    Catch ValidationError in check

--- src/argus/filter/checks.py
+++ src/argus/filter/checks.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.core.checks import Warning
 
+from rest_framework import serializers
+
 from argus.filter.validators import validate_jsonfilter
 
 
@@ -8,15 +10,16 @@ __all__ = ["fallback_filter_check"]
 
 
 def fallback_filter_check(app_configs, **kwargs):
-    errors = []
     fallback_filter = getattr(settings, "ARGUS_FALLBACK_FILTER", {})
-    if not validate_jsonfilter(fallback_filter):
-        errors.append(
-            Warning(
-                'The ARGUS_FALLBACK_FILTER setting is invalid and has been set to "{}"',
-                hint="See the docs for the format of the ARGUS_FALLBACK_FILTER setting",
-                obj=fallback_filter,
-                id="argus_filter.W001",
-            )
+    try:
+        validate_jsonfilter(fallback_filter)
+    except serializers.ValidationError:
+        warning = Warning(
+            'The ARGUS_FALLBACK_FILTER setting is invalid and has been set to "{}"',
+            hint="See the docs for the format of the ARGUS_FALLBACK_FILTER setting",
+            obj=fallback_filter,
+            id="argus_filter.W001",
         )
-    return errors
+        return [warning]
+    else:
+        return []
