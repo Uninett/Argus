7fef1757bb73535c9e7d48af5173f83256c2fa37
Simen Abelsen <simen.abelsen@sikt.no>
2025-12-09 19:30:13 +0100

    Add preview for incident table preferences (#1658)
    * Add preview for incident table preferences
    
    * Add tests for preferences and incident table preview views
    
    fix tests, control auth backend!
    
    * Use column constant in table context preview
    
    * Improve clarity in test names
    
    * Fix z-index bug for notifications and table icon tooltips

--- src/argus/htmx/user/views.py
+++ src/argus/htmx/user/views.py
@@ -5,9 +5,10 @@ from django.views.decorators.http import require_GET, require_POST
 from django_htmx.http import HttpResponseClientRefresh
 
 from argus.auth.utils import get_preference_obj, save_preferences
+from argus.htmx.incident.columns import BUILTIN_COLUMN_LAYOUT_NAME, get_incident_table_columns
 from argus.htmx.incident.views import HtmxHttpRequest
 from argus.htmx.user.preferences.utils import setup_preference_forms
-
+from argus.incident.models import Incident
 
 THEME_PREFERENCES = ["theme", "datetime_format_name"]
 
@@ -15,19 +16,51 @@ THEME_PREFERENCES = ["theme", "datetime_format_name"]
 @require_GET
 def user_preferences(request) -> HttpResponse:
     """Renders the main preferences page for a user"""
-
     forms = setup_preference_forms(request)
     theme_forms = [form for name, form in forms.items() if name in THEME_PREFERENCES]
     incident_forms = [form for name, form in forms.items() if name not in THEME_PREFERENCES]
+
+    preferences = request.user.get_preferences_context()
+    preferences_data = preferences.get("argus_htmx", {})
 
     context = {
         "page_title": "User preferences",
         "forms": forms,
         "theme_forms": theme_forms,
         "incident_forms": incident_forms,
+        **_get_incident_table_context(preferences_data),
     }
 
     return render(request, "htmx/user/preferences_list.html", context=context)
+
+
+@require_GET
+def incident_table_preview(request) -> HttpResponse:
+    """Render a preview of the incident table based on selected preferences."""
+    preferences = request.user.get_preferences_context()
+    preferences_data = preferences.get("argus_htmx", {})
+    preferences_data.update(request.GET.dict())
+
+    context = {
+        **_get_incident_table_context(preferences_data),
+        "preferences": preferences,
+    }
+
+    return render(request, "htmx/incident/_unpaged_incident_table.html", context=context)
+
+
+def _get_incident_table_context(preferences_data: dict) -> dict:
+    """Extract common logic for incident table rendering."""
+    selected_layout = preferences_data.get("incidents_table_layout", "standard")
+    preferences_data["incidents_table_layout_compact"] = selected_layout == "compact"
+    column_layout_name = preferences_data.get("incidents_table_column_name", BUILTIN_COLUMN_LAYOUT_NAME)
+    columns = get_incident_table_columns(column_layout_name)
+
+    return {
+        "dummy_column": True,
+        "columns": columns,
+        "incident_list": Incident.objects.all()[:1],
+    }
 
 
 @require_POST
