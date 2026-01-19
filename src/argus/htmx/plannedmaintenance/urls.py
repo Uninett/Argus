d1c848637b43a6f3ead5f481700c2e040711b935
Hanne Moa <hanne.moa@sikt.no>
2026-01-19 12:36:23 +0100

    add auth for views that alters pms

--- src/argus/htmx/plannedmaintenance/urls.py
+++ src/argus/htmx/plannedmaintenance/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = "htmx"
 urlpatterns = [
     path("create/", views.PlannedMaintenanceCreateView.as_view(), name="plannedmaintenance-create"),
-    path("<pk>/cancel/", views.cancel_planned_maintenance, name="plannedmaintenance-cancel"),
+    path("<pk>/cancel/", views.PlannedMaintenanceCancelView.as_view(), name="plannedmaintenance-cancel"),
     path("<pk>/delete/", views.PlannedMaintenanceDeleteView.as_view(), name="plannedmaintenance-delete"),
     path("<pk>/update/", views.PlannedMaintenanceUpdateView.as_view(), name="plannedmaintenance-update"),
     path("", views.PlannedMaintenanceListView.as_view(), name="plannedmaintenance-list"),
-    #     path("<pk>/", views.PlannedMaintenanceDetailView.as_view(), name="plannedmaintenance-detail"),
+    # path("<pk>/", views.PlannedMaintenanceDetailView.as_view(), name="plannedmaintenance-detail"),
 ]
