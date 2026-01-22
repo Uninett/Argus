ae1fc7469d7496339cfb510d8b1b90e3096c8b80
Hanne Moa <hanne.moa@sikt.no>
2026-01-22 12:37:19 +0100

    for completion

--- src/argus/htmx/plannedmaintenance/urls.py
+++ src/argus/htmx/plannedmaintenance/urls.py
@@ -7,9 +7,10 @@ app_name = "htmx"
 urlpatterns = [
     path("create/", views.PlannedMaintenanceCreateView.as_view(), name="plannedmaintenance-create"),
     path("search-filters/", views.search_filters, name="search-filters"),
+    re_path(r"^(?P<tab>upcoming|past)?/$", views.PlannedMaintenanceListView.as_view(), name="plannedmaintenance-list"),
     path("<pk>/cancel/", views.PlannedMaintenanceCancelView.as_view(), name="plannedmaintenance-cancel"),
     path("<pk>/delete/", views.PlannedMaintenanceDeleteView.as_view(), name="plannedmaintenance-delete"),
     path("<pk>/update/", views.PlannedMaintenanceUpdateView.as_view(), name="plannedmaintenance-update"),
     path("<pk>/", views.PlannedMaintenanceDetailView.as_view(), name="plannedmaintenance-detail"),
-    re_path(r"^(?P<tab>upcoming|past)?$", views.PlannedMaintenanceListView.as_view(), name="plannedmaintenance-list"),
+    path("", views.PlannedMaintenanceListView.as_view(), name="plannedmaintenance-list-upcoming"),
 ]
