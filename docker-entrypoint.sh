118d545054f8d149a15ddd5e7d960def996abf79
Simon Oliver Tveit <simtve@uninett.no>
2026-01-29 08:14:04 +0100

    Add admin user from environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,16 @@ git config --global --add safe.directory /argus
 python3 -m pip install -e .
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
+
+# Stop password from being printed in logs
+set +x
+if [ -n "$ADMIN_USERNAME" ] && [ -n "$ADMIN_PASSWORD" ]; then
+     if [ -n "$ADMIN_EMAIL" ]; then
+         python3 manage.py initial_setup -u $ADMIN_USERNAME -p $ADMIN_PASSWORD -e $ADMIN_EMAIL
+     else
+          python3 manage.py initial_setup -u $ADMIN_USERNAME -p $ADMIN_PASSWORD
+     fi
+fi
+set -x
+
 exec gunicorn -b 0.0.0.0 -p 8000 argus.site.wsgi:application
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,18 @@
 
 django-admin collectstatic --noinput
 django-admin migrate --noinput
+
+# Stop password from being printed in logs
+set +x
+if [ -n "$ADMIN_USERNAME" ] && [ -n "$ADMIN_PASSWORD" ]; then
+     if [ -n "$ADMIN_EMAIL" ]; then
+         python3 manage.py initial_setup -u $ADMIN_USERNAME -p $ADMIN_PASSWORD -e $ADMIN_EMAIL
+     else
+          python3 manage.py initial_setup -u $ADMIN_USERNAME -p $ADMIN_PASSWORD
+     fi
+fi
+set -x
+
 exec gunicorn \
      argus.site.wsgi:application \
      --forwarded-allow-ips="*" \
