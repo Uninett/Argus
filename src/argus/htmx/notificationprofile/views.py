696548f0c86ea9ac35c72e894c5342a62a2acfc2
Simen Abelsen <simen.abelsen@sikt.no>
2026-02-23 09:20:22 +0100

    Add collapsible cards and Choices.js selects for notification profiles

--- src/argus/htmx/notificationprofile/views.py
+++ src/argus/htmx/notificationprofile/views.py
@@ -6,12 +6,15 @@ See https://ccbv.co.uk/ to grok class-based views.
 
 from django import forms
 from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from django.template.response import TemplateResponse
 from django.urls import reverse
+from django.utils import timezone
 from django.views.decorators.http import require_GET
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django_htmx.http import HttpResponseClientRedirect
 
 from argus.htmx.request import HtmxHttpRequest
-from argus.htmx.widgets import DropdownMultiSelect
 from argus.notificationprofile.media import MEDIA_CLASSES_DICT
 from argus.htmx.modals import DeleteModal
 from argus.notificationprofile.models import NotificationProfile, Timeslot, Filter, DestinationConfig
@@ -35,20 +38,10 @@ class DestinationFieldMixin:
     def _init_destinations(self, user):
         qs = DestinationConfig.objects.filter(user=user)
         self.fields["destinations"].queryset = qs
-        if self.instance.id:
-            partial_get = reverse(
-                "htmx:notificationprofile-destinations-field-update",
-                kwargs={"pk": self.instance.pk},
-            )
-        else:
-            partial_get = reverse("htmx:notificationprofile-destinations-field-create")
-        self.fields["destinations"].widget = DropdownMultiSelect(
-            partial_get=partial_get,
-            attrs={
-                "placeholder": "select destination...",
-                "field_styles": "input input-bordered border-b max-w-xs",
-            },
+        self.fields["destinations"].widget = forms.SelectMultiple(
+            attrs={"placeholder": "Select destinations..."},
         )
+        self.fields["destinations"].widget.template_name = "htmx/forms/choices_select_multiple.html"
         self.fields["destinations"].choices = self._get_destination_choices(user)
 
 
@@ -56,21 +49,10 @@ class FilterFieldMixin:
     def _init_filters(self, user):
         qs = Filter.objects.usable_by(user=user)
         self.fields["filters"].queryset = qs
-
-        if self.instance.id:
-            partial_get = reverse(
-                "htmx:notificationprofile-filters-field-update",
-                kwargs={"pk": self.instance.pk},
-            )
-        else:
-            partial_get = reverse("htmx:notificationprofile-filters-field-create")
-        self.fields["filters"].widget = DropdownMultiSelect(
-            partial_get=partial_get,
-            attrs={
-                "placeholder": "select filter...",
-                "field_styles": "input input-bordered border-b max-w-xs",
-            },
+        self.fields["filters"].widget = forms.SelectMultiple(
+            attrs={"placeholder": "Select filters..."},
         )
+        self.fields["filters"].widget.template_name = "htmx/forms/choices_select_multiple.html"
         self.fields["filters"].choices = tuple(qs.values_list("id", "name"))
 
 
@@ -172,14 +154,23 @@ class NotificationProfileMixin:
 
     model = NotificationProfile
 
-    def _set_delete_modal(self, form, obj):
-        form.modal = DeleteModal(
+    def _make_delete_modal(self, obj, **kwargs):
+        return DeleteModal(
             header="Delete notification profile",
+            button_title="Delete",
+            button_class="btn-sm btn-outline btn-error",
             explanation=f'Delete the notification profile "{obj}"?',
             dialog_id=f"delete-modal-{obj.pk}",
             endpoint=reverse("htmx:notificationprofile-delete", kwargs={"pk": obj.pk}),
+            **kwargs,
         )
+
+    def _set_delete_modal(self, form, obj):
+        form.modal = self._make_delete_modal(obj)
         return form
+
+    def _attach_card_delete_modal(self, obj):
+        obj.delete_modal = self._make_delete_modal(obj, opener_button_template_name="htmx/_blank.html")
 
     def get_queryset(self):
         qs = (
@@ -188,14 +179,13 @@ class NotificationProfileMixin:
             .select_related("timeslot")
             .prefetch_related(
                 "filters",
-                "destinations",
+                "destinations__media",
+                "timeslot__time_recurrences",
             )
         )
         return qs.filter(user_id=self.request.user.id)
 
     def get_template_names(self):
-        if self.request.htmx and hasattr(self, "partial_template_name"):
-            return [self.partial_template_name]
         orig_app_label = self.model._meta.app_label
         orig_model_name = self.model._meta.model_name
         self.model._meta.app_label = "htmx/notificationprofile"
@@ -219,7 +209,6 @@ class ChangeMixin:
     "Common functionality for create and update views"
 
     form_class = NotificationProfileForm
-    partial_template_name = "htmx/notificationprofile/_notificationprofile_form.html"
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -229,7 +218,14 @@ class ChangeMixin:
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
-        return super().form_valid(form)
+        self.object.save()
+        form.save_m2m()
+        return self.form_valid_response()
+
+    def form_valid_response(self):
+        if self.request.htmx:
+            return HttpResponseClientRedirect(self.get_success_url())
+        return HttpResponseRedirect(self.get_success_url())
 
     def get_prefix(self):
         if self.object and self.object.pk:
@@ -241,31 +237,56 @@ class ChangeMixin:
 class NotificationProfileListView(NotificationProfileMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        forms = []
-        for obj in self.get_queryset():
-            form = NotificationProfileForm(None, prefix=f"npf{obj.pk}", user=self.request.user, instance=obj)
-            form = self._set_delete_modal(form, obj)
-            forms.append(form)
-        context["form_list"] = forms
+        for obj in context["object_list"]:
+            self._attach_card_delete_modal(obj)
         return context
 
 
 class NotificationProfileDetailView(NotificationProfileMixin, DetailView):
     def dispatch(self, request, *args, **kwargs):
-        object = self.get_object()
-        return redirect("htmx:notificationprofile-update", pk=object.pk)
+        obj = self.get_object()
+        if request.htmx:
+            self._attach_card_delete_modal(obj)
+            return TemplateResponse(
+                request,
+                "htmx/notificationprofile/_notificationprofile_card.html",
+                {"profile": obj},
+            )
+        return redirect("htmx:notificationprofile-update", pk=obj.pk)
 
 
 class NotificationProfileCreateView(ChangeMixin, NotificationProfileMixin, CreateView):
-    pass
+    def get_template_names(self):
+        if self.request.htmx:
+            return ["htmx/notificationprofile/_notificationprofile_form.html"]
+        return super().get_template_names()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["is_create"] = True
+        return context
 
 
 class NotificationProfileUpdateView(ChangeMixin, NotificationProfileMixin, UpdateView):
+    def get_template_names(self):
+        if self.request.htmx:
+            return ["htmx/notificationprofile/_notificationprofile_form_content.html"]
+        return super().get_template_names()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         form = context["form"]
         context["form"] = self._set_delete_modal(form, self.object)
         return context
+
+    def form_valid_response(self):
+        if self.request.htmx:
+            context = self.get_context_data()
+            context["profile"] = self.object
+            now = timezone.localtime().strftime("%H:%M:%S")
+            context["success_message"] = f'Saved profile "{self.object}" at {now}.'
+            return self.render_to_response(context)
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class NotificationProfileDeleteView(NotificationProfileMixin, DeleteView):
