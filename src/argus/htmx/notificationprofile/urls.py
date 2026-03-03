9f98f3e7c724eac42e1e5805be5b67dcb66fd4ef
Simen Abelsen <simen.abelsen@sikt.no>
2026-03-03 10:56:57 +0100

    Improve notification profile form design (#1804)
    * Add collapsible cards and Choices.js selects for notification profiles
    
    * Rearrange notification profile form layout and inline action buttons
    
    * Make tests less brittle and superfluous
    
    * Clean up notification profile templates and remove dead code
    
    * Move profile create button next to tab bar
    
    * Address PR review feedback
    
    * Add changelog entry for #1796
    
    * Address PR review feedback for profile forms

--- src/argus/htmx/notificationprofile/urls.py
+++ src/argus/htmx/notificationprofile/urls.py
@@ -7,19 +7,7 @@ app_name = "htmx"
 urlpatterns = [
     path("", views.NotificationProfileListView.as_view(), name="notificationprofile-list"),
     path("profiles/create/", views.NotificationProfileCreateView.as_view(), name="notificationprofile-create"),
-    path("profiles/field/filters/", views.filters_form_view, name="notificationprofile-filters-field-create"),
-    path(
-        "profiles/field/destinations/",
-        views.destinations_form_view,
-        name="notificationprofile-destinations-field-create",
-    ),
     path("profiles/<pk>/", views.NotificationProfileDetailView.as_view(), name="notificationprofile-detail"),
     path("profiles/<pk>/update/", views.NotificationProfileUpdateView.as_view(), name="notificationprofile-update"),
     path("profiles/<pk>/delete/", views.NotificationProfileDeleteView.as_view(), name="notificationprofile-delete"),
-    path("profiles/<pk>/field/filters/", views.filters_form_view, name="notificationprofile-filters-field-update"),
-    path(
-        "profiles/<pk>/field/destinations/",
-        views.destinations_form_view,
-        name="notificationprofile-destinations-field-update",
-    ),
 ]
--- tests/htmx/notificationprofile/test_views.py
+++ tests/htmx/notificationprofile/test_views.py
@@ -2,6 +2,11 @@ from django.test import TestCase, override_settings
 from django.urls import reverse
 
 from argus.auth.factories import PersonUserFactory
+from argus.notificationprofile.factories import (
+    NotificationProfileFactory,
+    TimeslotFactory,
+)
+from argus.filter.factories import FilterFactory
 
 
 @override_settings(AUTHENTICATION_BACKENDS=["django.contrib.auth.backends.ModelBackend"])
@@ -27,3 +32,135 @@ class TestNotificationProfileListView(NotificationProfileViewTestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["page_title"], "Profiles")
+
+    def test_when_get_it_should_list_profiles(self):
+        timeslot = TimeslotFactory(user=self.user)
+        NotificationProfileFactory(user=self.user, timeslot=timeslot)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["object_list"]), 1)
+
+
+class TestNotificationProfileDetailView(NotificationProfileViewTestCase):
+    def setUp(self):
+        super().setUp()
+        timeslot = TimeslotFactory(user=self.user)
+        self.profile = NotificationProfileFactory(user=self.user, timeslot=timeslot)
+        self.url = reverse("htmx:notificationprofile-detail", kwargs={"pk": self.profile.pk})
+
+    def test_given_htmx_get_it_should_return_card_partial(self):
+        response = self.client.get(self.url, HTTP_HX_REQUEST="true")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "htmx/notificationprofile/_notificationprofile_card.html")
+
+    def test_when_non_htmx_get_it_should_redirect_to_update(self):
+        response = self.client.get(self.url)
+
+        self.assertRedirects(
+            response,
+            reverse("htmx:notificationprofile-update", kwargs={"pk": self.profile.pk}),
+            fetch_redirect_response=False,
+        )
+
+
+class TestNotificationProfileCreateView(NotificationProfileViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self.timeslot = TimeslotFactory(user=self.user)
+        self.filter = FilterFactory(user=self.user, name="Test Filter", filter={})
+        self.url = reverse("htmx:notificationprofile-create")
+
+    def _build_post_data(self, **overrides):
+        data = {
+            "name": "Test Profile",
+            "timeslot": self.timeslot.pk,
+            "filters": [self.filter.pk],
+            "destinations": [],
+            "active": "on",
+        }
+        data.update(overrides)
+        return data
+
+    def test_when_get_it_should_render_form(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("form", response.context)
+
+    def test_given_htmx_get_it_should_return_form_partial(self):
+        response = self.client.get(self.url, HTTP_HX_REQUEST="true")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "htmx/notificationprofile/_notificationprofile_form.html")
+        self.assertTrue(response.context["is_create"])
+
+    def test_when_valid_post_it_should_redirect_to_list(self):
+        response = self.client.post(self.url, data=self._build_post_data())
+
+        self.assertRedirects(response, reverse("htmx:notificationprofile-list"), fetch_redirect_response=False)
+
+    def test_given_htmx_valid_post_it_should_redirect_via_hx_redirect(self):
+        response = self.client.post(self.url, data=self._build_post_data(), HTTP_HX_REQUEST="true")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("HX-Redirect", response.headers)
+
+
+class TestNotificationProfileUpdateView(NotificationProfileViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self.timeslot = TimeslotFactory(user=self.user)
+        self.filter = FilterFactory(user=self.user, name="Test Filter", filter={})
+        self.profile = NotificationProfileFactory(user=self.user, timeslot=self.timeslot, name="Test Profile")
+        self.profile.filters.add(self.filter)
+        self.url = reverse("htmx:notificationprofile-update", kwargs={"pk": self.profile.pk})
+
+    def test_when_get_it_should_render_form(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("form", response.context)
+
+    def _build_post_data(self, **overrides):
+        prefix = f"npf{self.profile.pk}"
+        data = {
+            f"{prefix}-name": self.profile.name,
+            f"{prefix}-timeslot": self.timeslot.pk,
+            f"{prefix}-filters": [self.filter.pk],
+            f"{prefix}-destinations": [],
+            f"{prefix}-active": "on",
+        }
+        data.update(overrides)
+        return data
+
+    def test_given_htmx_valid_post_it_should_persist_and_render_success(self):
+        prefix = f"npf{self.profile.pk}"
+        post_data = self._build_post_data(**{f"{prefix}-name": "Updated Name"})
+
+        response = self.client.post(self.url, data=post_data, HTTP_HX_REQUEST="true")
+
+        self.assertEqual(response.status_code, 200)
+        self.profile.refresh_from_db()
+        self.assertEqual(self.profile.name, "Updated Name")
+        self.assertIn("success_message", response.context)
+
+    def test_given_htmx_invalid_post_it_should_return_form_content_partial_with_errors(self):
+        prefix = f"npf{self.profile.pk}"
+        post_data = self._build_post_data(**{f"{prefix}-timeslot": ""})
+        del post_data[f"{prefix}-active"]
+
+        response = self.client.post(self.url, data=post_data, HTTP_HX_REQUEST="true")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "htmx/notificationprofile/_notificationprofile_form_content.html")
+        self.assertTrue(response.context["form"].errors)
+
+    def test_when_valid_post_it_should_redirect_to_list(self):
+        post_data = self._build_post_data()
+
+        response = self.client.post(self.url, data=post_data)
+
+        self.assertRedirects(response, reverse("htmx:notificationprofile-list"), fetch_redirect_response=False)
