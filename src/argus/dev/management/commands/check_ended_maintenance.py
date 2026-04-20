686ad9e2a552cd47b2ef265fa34282dd536ff000
Johanna England <johanna.england@sikt.no>
2026-04-20 14:22:18 +0200

    Remove linked incidents for all ended PM tasks
    Previously if this was set up as a cron job incidents were not removed if the end date was longer than x minutes ago

--- src/argus/dev/management/commands/check_ended_maintenance.py
+++ src/argus/dev/management/commands/check_ended_maintenance.py
@@ -1,26 +1,13 @@
-from datetime import timedelta
-
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 
 from argus.plannedmaintenance.models import PlannedMaintenanceTask
 
 
 class Command(BaseCommand):
-    help = "Find all planned maintenance tasks that have ended recently and remove the incidents connection"
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "-m",
-            "--minutes",
-            default=5,
-            type=int,
-            help="Only remove the incidents from the planned maintenance tasks that have ended within the last <number> minutes",
-        )
+    help = "Removes incidents from ended planned maintenance tasks"
 
     def handle(self, *args, **options):
-        end_time = timezone.now() - timedelta(minutes=options.get("minutes"))
-        ended_pm_tasks = PlannedMaintenanceTask.objects.ended_after_time(end_time)
+        ended_pm_tasks_with_linked_incidents = PlannedMaintenanceTask.objects.past().exclude(incidents=None)
 
-        for pm_task in ended_pm_tasks:
+        for pm_task in ended_pm_tasks_with_linked_incidents:
             pm_task.incidents.clear()
