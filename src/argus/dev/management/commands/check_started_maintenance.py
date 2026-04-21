cfb11f2af93e116ada55173a4fa2545507c99394
Aleksander Fløtten <aleksander.flotten@sikt.no>
2026-04-21 13:11:34 +0200

    Implement feedback

--- src/argus/dev/management/commands/check_started_maintenance.py
+++ src/argus/dev/management/commands/check_started_maintenance.py
@@ -4,11 +4,8 @@ from argus.plannedmaintenance.models import PlannedMaintenanceTask
 
 
 class Command(BaseCommand):
-    help = "Find all current and future planned maintenance tasks and connect them with incidents that are hit by them"
+    help = "Find all current planned maintenance tasks and connect them with incidents that are hit by them"
 
     def handle(self, *args, **options):
-        current_pm_tasks = PlannedMaintenanceTask.objects.current()
-        future_pm_tasks = PlannedMaintenanceTask.objects.future()
-
-        for pm_task in current_pm_tasks.union(future_pm_tasks):
+        for pm_task in PlannedMaintenanceTask.objects.current():
             pm_task.connect_covered_incidents()
--- tests/dev/test_check_started_maintenance.py
+++ tests/dev/test_check_started_maintenance.py
@@ -7,7 +7,9 @@ from django.utils import timezone
 
 from argus.filter.factories import FilterFactory
 from argus.incident.factories import StatefulIncidentFactory
+from argus.incident.models import Incident
 from argus.plannedmaintenance.factories import PlannedMaintenanceFactory
+from argus.plannedmaintenance.utils import incidents_covered_by_planned_maintenance_task
 from argus.util.datetime_utils import LOCAL_INFINITY
 from argus.util.testing import connect_signals, disconnect_signals
 
@@ -19,7 +21,7 @@ class CheckStartedMaintenanceTests(TestCase):
         now = timezone.now()
 
         self.open_filter = FilterFactory(filter={"open": True})
-        self.incident = self.incident = StatefulIncidentFactory(start_time=now - timedelta(days=1))
+        self.incident = StatefulIncidentFactory(start_time=now - timedelta(days=1))
 
         self.recently_started_pm = PlannedMaintenanceFactory(
             start_time=now - timedelta(minutes=1),
@@ -64,13 +66,19 @@ class CheckStartedMaintenanceTests(TestCase):
         out = self.call_command()
 
         self.assertFalse(out)
-        self.assertIn(self.incident, self.recently_started_pm.incidents)
+        incidents = incidents_covered_by_planned_maintenance_task(
+            queryset=Incident.objects.all(), pm_task=self.recently_started_pm
+        )
+        self.assertIn(self.incident, incidents)
 
     def test_given_long_ago_started_pm_then_link_incident(self):
         out = self.call_command()
 
         self.assertFalse(out)
-        self.assertIn(self.incident, self.long_ago_started_pm.incidents)
+        incidents = incidents_covered_by_planned_maintenance_task(
+            queryset=Incident.objects.all(), pm_task=self.long_ago_started_pm
+        )
+        self.assertIn(self.incident, incidents)
 
     def test_given_past_pm_then_do_not_link_incident(self):
         out = self.call_command()
