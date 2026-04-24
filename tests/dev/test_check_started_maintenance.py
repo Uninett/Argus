ed27efec28c6968161b8144ce5279dd7620e1f3d
Aleksander Fløtten <aleksander.flotten@sikt.no>
2026-04-24 15:06:44 +0200

    Update tests

--- tests/dev/test_check_started_maintenance.py
+++ tests/dev/test_check_started_maintenance.py
@@ -7,9 +7,7 @@ from django.utils import timezone
 
 from argus.filter.factories import FilterFactory
 from argus.incident.factories import StatefulIncidentFactory
-from argus.incident.models import Incident
 from argus.plannedmaintenance.factories import PlannedMaintenanceFactory
-from argus.plannedmaintenance.utils import incidents_covered_by_planned_maintenance_task
 from argus.util.datetime_utils import LOCAL_INFINITY
 from argus.util.testing import connect_signals, disconnect_signals
 
@@ -66,19 +64,13 @@ class CheckStartedMaintenanceTests(TestCase):
         out = self.call_command()
 
         self.assertFalse(out)
-        incidents = incidents_covered_by_planned_maintenance_task(
-            queryset=Incident.objects.all(), pm_task=self.recently_started_pm
-        )
-        self.assertIn(self.incident, incidents)
+        self.assertIn(self.incident, self.recently_started_pm.incidents.all())
 
     def test_given_long_ago_started_pm_then_link_incident(self):
         out = self.call_command()
 
         self.assertFalse(out)
-        incidents = incidents_covered_by_planned_maintenance_task(
-            queryset=Incident.objects.all(), pm_task=self.long_ago_started_pm
-        )
-        self.assertIn(self.incident, incidents)
+        self.assertIn(self.incident, self.long_ago_started_pm.incidents.all())
 
     def test_given_past_pm_then_do_not_link_incident(self):
         out = self.call_command()
