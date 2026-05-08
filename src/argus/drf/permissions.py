cb1a5b2af686c92e0230d6ce22b31cf0252dd4e4
Johanna England <johanna.england@sikt.no>
2026-05-08 16:10:09 +0200

    Add permission class for is admin or read only

--- src/argus/drf/permissions.py
+++ src/argus/drf/permissions.py
@@ -14,6 +14,14 @@ class IsSuperuserOrReadOnly(permissions.BasePermission):
         return request.user.is_superuser
 
 
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_staff
+
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
