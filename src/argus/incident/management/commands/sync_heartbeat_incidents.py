ae446d00445b5215c4f5f3bc1b023f59850b2790
Hanne Moa <hanne.moa@sikt.no>
2026-07-02 13:19:31 +0200

    fixup! Add command for opening/closing heartbeat incidents

--- src/argus/incident/management/commands/sync_heartbeat_incidents.py
+++ src/argus/incident/management/commands/sync_heartbeat_incidents.py
@@ -7,16 +7,33 @@ class Command(BaseCommand):
     help = "Check that heartbeat-supporting sources are alive"
 
     def handle(self, *args, **options):
-        alive_sources, new_incidents = sync_heartbeats_with_heartbeat_incidents()
-        if not (alive_sources or new_incidents):
+        alive_sources, new_incidents, remaining_incidents = sync_heartbeats_with_heartbeat_incidents()
+        if not (alive_sources or new_incidents or remaining_incidents):
             return
+        count_closed = len(alive_sources)
+        count_incidents = len(new_incidents)
+        count_dead = len(remaining_incidents)
+
         if options["verbosity"] > 0:
-            for source in alive_sources:
-                self.stdout.write(f'Closed incident for source "{source.name}", it\'s back')
-            for incident in new_incidents:
-                self.stdout.write(f"Created incident {incident}")
-        else:
-            count_incidents = len(new_incidents)
-            self.stdout.write(f"Created {count_incidents} new incidents")
-            count_alive = len(alive_sources)
-            self.stdout.write(f"Closed {count_alive} existing incidents")
+            self.stdout.write(
+                f"Heartbeat incidents: Created: {count_incidents}, Closed: {count_closed}, Remaining: {count_dead}"
+            )
+
+        if options["verbosity"] > 1:
+            if count_incidents:
+                self.stdout.write()
+                self.stdout.write("Created incidents:")
+                for incident in new_incidents:
+                    self.stdout.write(f"- {incident}")
+
+            if count_closed:
+                self.stdout.write()
+                self.stdout.write("Closed incidents:")
+                for source in alive_sources:
+                    self.stdout.write(f"- {source.name}")
+
+            if count_dead:
+                self.stdout.write()
+                self.stdout.write("Remaining incidents:")
+                for incident in remaining_incidents:
+                    self.stdout.write(f"- {incident}")
--- tests/incident/management_commands/test_sync_heartbeat_incidents.py
+++ tests/incident/management_commands/test_sync_heartbeat_incidents.py
@@ -12,58 +12,84 @@ class TestSyncHeartbeatIncidents(TestCase):
     def setUp(self):
         get_or_create_default_instances()
 
-    def test_when_no_sources_then_print_nothing(self):
+    def test_when_nothing_to_report_and_not_verbose_then_print_nothing(self):
+        # No verbosity flag
         F = StringIO()
 
         with contextlib.redirect_stdout(F):
-            call_command("sync_heartbeat_incidents", verbosity=0)
+            call_command("sync_heartbeat_incidents", verbosity=1)
 
         output = F.getvalue().strip()
         self.assertFalse(output)
 
-    def test_when_no_sources_and_verbose_then_print_nothing(self):
+    def test_when_nothing_to_report_and_verbose_then_print_nothing(self):
+        # Flag "-v 2"
         F = StringIO()
 
-        call_command("sync_heartbeat_incidents", verbosity=1)
+        with contextlib.redirect_stdout(F):
+            call_command("sync_heartbeat_incidents", verbosity=2)
 
         output = F.getvalue().strip()
         self.assertFalse(output)
 
-    def test_when_new_dead_source_incidents_then_print_two_lines(self):
+    def test_when_something_to_report_but_quiet_print_nothing(self):
+        # Flag "-v 0"
         F = StringIO()
 
         alive_sources = []
         new_incidents = ["golgamfrincham"]
+        remaining_incidents = []
         with patch(
             "argus.incident.management.commands.sync_heartbeat_incidents.sync_heartbeats_with_heartbeat_incidents",
-            return_value=(alive_sources, new_incidents),
+            return_value=(alive_sources, new_incidents, remaining_incidents),
         ):
             with contextlib.redirect_stdout(F):
                 call_command("sync_heartbeat_incidents", verbosity=0)
 
-        output = F.getvalue().strip()
-        self.assertTrue(output)
-        new, old = output.split("\n")
-        self.assertEqual(new, "Created 1 new incidents")
-        self.assertEqual(old, "Closed 0 existing incidents")
+        output = F.getvalue()
+        self.assertFalse(output)
 
-    def test_when_new_dead_source_incidents_and_verbose_then_print_one_line(self):
+    def test_when_new_dead_source_incidents_then_print_overview_line(self):
+        # No verbosity flag
         F = StringIO()
 
         alive_sources = []
-        new_incidents = ["pollywog"]
+        new_incidents = ["golgamfrincham"]
+        remaining_incidents = []
         with patch(
             "argus.incident.management.commands.sync_heartbeat_incidents.sync_heartbeats_with_heartbeat_incidents",
-            return_value=(alive_sources, new_incidents),
+            return_value=(alive_sources, new_incidents, remaining_incidents),
         ):
             with contextlib.redirect_stdout(F):
                 call_command("sync_heartbeat_incidents", verbosity=1)
 
         output = F.getvalue().strip()
-        self.assertTrue(output)
-        self.assertEqual(output, "Created incident pollywog")
+        self.assertEqual(
+            output,
+            "Heartbeat incidents: Created: 1, Closed: 0, Remaining: 0",
+        )
 
-    def test_when_reanimated_source_then_print_two_lines(self):
+    def test_when_new_dead_source_incidents_and_verbose_then_print_incident_in_addition_to_overview(self):
+        # Flag "-v 2"
+        F = StringIO()
+
+        alive_sources = []
+        new_incidents = ["pollywog"]
+        remaining_incidents = []
+        with patch(
+            "argus.incident.management.commands.sync_heartbeat_incidents.sync_heartbeats_with_heartbeat_incidents",
+            return_value=(alive_sources, new_incidents, remaining_incidents),
+        ):
+            with contextlib.redirect_stdout(F):
+                call_command("sync_heartbeat_incidents", verbosity=2)
+
+        output = F.getvalue()
+        self.assertEqual(
+            output, ("Heartbeat incidents: Created: 1, Closed: 0, Remaining: 0\n\nCreated incidents:\n- pollywog\n")
+        )
+
+    def test_when_reanimated_source_then_print_overview_line(self):
+        # No verbosity flag
         F = StringIO()
 
         class FakeSource:
@@ -71,20 +97,19 @@ class TestSyncHeartbeatIncidents(TestCase):
 
         alive_sources = [FakeSource]
         new_incidents = []
+        remaining_incidents = []
         with patch(
             "argus.incident.management.commands.sync_heartbeat_incidents.sync_heartbeats_with_heartbeat_incidents",
-            return_value=(alive_sources, new_incidents),
+            return_value=(alive_sources, new_incidents, remaining_incidents),
         ):
             with contextlib.redirect_stdout(F):
-                call_command("sync_heartbeat_incidents", verbosity=0)
+                call_command("sync_heartbeat_incidents", verbosity=1)
 
         output = F.getvalue().strip()
-        self.assertTrue(output)
-        new, old = output.split("\n")
-        self.assertEqual(new, "Created 0 new incidents")
-        self.assertEqual(old, "Closed 1 existing incidents")
+        self.assertEqual(output, "Heartbeat incidents: Created: 0, Closed: 1, Remaining: 0")
 
-    def test_when_new_dead_source_incidents_and_verbose_then_print_two_lines(self):
+    def test_when_reanimated_source_and_verbose_then_print_source_name_in_addition_to_overview(self):
+        # Flag "-v 2"
         F = StringIO()
 
         class FakeSource:
@@ -92,13 +117,15 @@ class TestSyncHeartbeatIncidents(TestCase):
 
         alive_sources = [FakeSource]
         new_incidents = []
+        remaining_incidents = []
         with patch(
             "argus.incident.management.commands.sync_heartbeat_incidents.sync_heartbeats_with_heartbeat_incidents",
-            return_value=(alive_sources, new_incidents),
+            return_value=(alive_sources, new_incidents, remaining_incidents),
         ):
             with contextlib.redirect_stdout(F):
-                call_command("sync_heartbeat_incidents", verbosity=1)
+                call_command("sync_heartbeat_incidents", verbosity=2)
 
-        output = F.getvalue().strip()
-        self.assertTrue(output)
-        self.assertEqual(output, """Closed incident for source "oopsy", it's back""")
+        output = F.getvalue()
+        self.assertEqual(
+            output, ("Heartbeat incidents: Created: 0, Closed: 1, Remaining: 0\n\nClosed incidents:\n- oopsy\n")
+        )
