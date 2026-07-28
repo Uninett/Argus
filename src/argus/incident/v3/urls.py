a65cf791389f79ac3e84550895fd7252a56145e0
Hanne Moa <hanne.moa@sikt.no>
2026-07-28 14:37:21 +0200

    Fix tag rendering for incident tags subviews
    .. the huge test case for API integration tests has been broken up into
    one class per viewset and one class per extra action.

--- src/argus/incident/v3/urls.py
+++ src/argus/incident/v3/urls.py
@@ -11,7 +11,12 @@ router.register(r"", views.IncidentViewSet)
 
 sourced_incident_list = views.SourceLockedIncidentViewSet.as_view({"get": "list", "post": "create"})
 
+tag_list = views.IncidentTagViewSet.as_view({"get": "list", "post": "create"})
+tag_detail = views.IncidentTagViewSet.as_view({"delete": "destroy", "get": "retrieve"})
+
 app_name = "incident"
 urlpatterns = [
     path("mine/", sourced_incident_list, name="source_locked_incidents"),
+    path("<int:incident_pk>/tags/", tag_list, name="incident-tags"),
+    path("<int:incident_pk>/tags/<str:tag>/", tag_detail, name="incident-tag"),
 ] + router.urls
